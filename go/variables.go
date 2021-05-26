73220c3b9d4e2a442bbc0f911e6fad90f7bcadb2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-26 19:50:57 -0400

    Create variables.go

---
+++ go/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String
+	var firstName string = "John"
+	fmt.Println(firstName)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	// Bool
+	var validName string = true
+	fmt.Println(validName)
+	//
+	var e int
+	fmt.Println(e)
+	//
+	f := "apple"
+	fmt.Println(f)
+}
