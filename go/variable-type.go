271e6b9b3c4862ff1fba6625864090b32018d4e9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-29 02:14:39 -0400

    Update variable-type.go

--- go/variable-type.go
+++ go/variable-type.go
@@ -13,7 +13,7 @@ func main() {
 	tst5 := []string{"foo", "bar", "baz"}
 	tst6 := map[string]int{"apple": 23, "tomato": 13}
 
-	fmt.Printf("\n######################################\n")
+	fmt.Printf("\n")
 	fmt.Println(reflect.TypeOf(tst1))
 	fmt.Println(reflect.TypeOf(tst2))
 	fmt.Println(reflect.TypeOf(tst3))
@@ -21,7 +21,7 @@ func main() {
 	fmt.Println(reflect.TypeOf(tst5))
 	fmt.Println(reflect.TypeOf(tst6))
 
-	fmt.Printf("\n######################################\n")
+	fmt.Printf("\n")
 	fmt.Println(reflect.ValueOf(tst1).Kind())
 	fmt.Println(reflect.ValueOf(tst2).Kind())
 	fmt.Println(reflect.ValueOf(tst3).Kind())
@@ -29,7 +29,7 @@ func main() {
 	fmt.Println(reflect.ValueOf(tst5).Kind())
 	fmt.Println(reflect.ValueOf(tst6).Kind())
 
-	fmt.Printf("\n######################################\n")
+	fmt.Printf("\n")
 	fmt.Printf("%T\n", tst1)
 	fmt.Printf("%T\n", tst2)
 	fmt.Printf("%T\n", tst3)
