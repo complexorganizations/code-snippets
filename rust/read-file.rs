459ef1591fa3a2db82ac705b59f5979dc56748b5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-07 05:06:31 -0400

    Update read-file.rs

--- rust/read-file.rs
+++ rust/read-file.rs
@@ -2,10 +2,9 @@ use std::fs;
 
 fn main() {
     let filename = "example-file";
-    println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!("{}", contents);
 }
