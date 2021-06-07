644141a4b3c5e1118b233e1cbcef61942cc49b6d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-06 20:13:46 -0400

    Create read-file.rs

---
+++ rust/read-file.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    // --snip--
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
