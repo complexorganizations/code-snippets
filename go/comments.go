890ac268d16dc73c97243e5852d413a8bc891c95
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-07 19:08:20 -0400

    Create comments.go

---
+++ go/comments.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+// This is a single remark line.
+
+/* This is a comment block that can
+stretch onto numerous lines. */
+
+func main() {
+	fmt.Println("Hello, playground") // Here's another example
+}
