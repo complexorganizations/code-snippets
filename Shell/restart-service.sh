7007f7bbc83c532668744be5c84409efdf810108
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-07 19:19:16 -0400

    Create restart-service.sh

---
+++ Shell/restart-service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+  function service-manager() {
+    if pgrep systemd-journal; then
+      systemctl disable SERVICE_NAME
+      systemctl stop SERVICE_NAME
+    else
+      service SERVICE_NAME disable
+      service SERVICE_NAME stop
+    fi
+  }
+
+  service-manager
