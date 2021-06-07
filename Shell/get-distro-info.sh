6979d41ebc530e18e43544672f9d2b288b6e4e1c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-07 19:18:17 -0400

    Create get-distro-info.sh

---
+++ Shell/get-distro-info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Detect Operating System
+function dist-check() {
+  if [ -e /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    DISTRO=${ID}
+    DISTRO_VERSION=${VERSION_ID}
+  fi
+}
+
+# Check Operating System
+dist-check
+
+function print-info() {
+  echo "${DISTRO}"
+  echo "${DISTRO_VERSION}"
+}
+
+print-info
