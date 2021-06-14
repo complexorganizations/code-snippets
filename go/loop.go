b55ac1477643c720b6d7183114f0083feae112e7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-13 20:45:07 -0400

    Create loop.go

---
+++ go/loop.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// loop 10 times
+	for loop := 0; loop < 10; loop++ {
+		fmt.Println("This is a loop, and it will go around 10 times")
+	}
+	// loop 5 times
+	for loop := 0; loop < 5; loop++ {
+		fmt.Println("This is a loop, and it will go around 5 times")
+	}
+	// loop forever
+	for {
+		fmt.Println("This is a loop and it will go on forever")
+	}
+}
