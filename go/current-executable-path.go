735fa2dd96fab730a63782695a1a52781678cc63
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-14 12:56:15 -0400

    Update current-executable-path.go

--- go/current-executable-path.go
+++ go/current-executable-path.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 func main() {
 	currentExecutable, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
 	currentExecutablePath := filepath.Dir(currentExecutable)
+	if err != nil {
+		log.Println(err)
+	}
 	fmt.Println("Executable", currentExecutable)    // Executable /tmpfs/play
 	fmt.Println("Directory", currentExecutablePath) // Directory /tmpfs
 }
