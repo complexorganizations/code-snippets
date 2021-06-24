ce1469cadfbe188777dec993bd3b68445eb4ee59
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-24 08:31:49 -0400

    Update append.go

--- go/append.go
+++ go/append.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
-	filePath, err := os.OpenFile("file-name", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writeToFile("test", "content")
+}
+
+func writeToFile(pathInSystem string, content string) {
+	// open the file and if its not there create one.
+	filePath, err := os.OpenFile(pathInSystem, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
 	}
+	// write the content to the file
+	_, err = filePath.WriteString(content + "\n")
+	if err != nil {
+		log.Println(err)
+	}
+	// close the file
 	defer filePath.Close()
-	_, err = filePath.WriteString("text to append")
-	if err != nil {
-		log.Println(err)
-	}
 }
