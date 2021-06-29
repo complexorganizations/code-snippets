741bb058539fbefd1de286c2b26e2b98f1d9ffd3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-28 20:24:03 -0400

    Update remove-string-from-slice.go

--- go/remove-string-from-slice.go
+++ go/remove-string-from-slice.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 )
 
-func removeStringFromSlice(originalSlice []string, removeString string) []string {
-	for i := 0; i < len(originalSlice); i++ {
-		if originalSlice[i] == removeString {
-			return append(originalSlice[:i], originalSlice[i+1:]...)
-		}
-	}
-	return originalSlice
-}
-
 func main() {
 	sampleSlice := []string{"one", "two", "three"}
 	sampleSlice = removeStringFromSlice(sampleSlice, "two")
 	fmt.Println(sampleSlice)
+}
+
+func removeStringFromSlice(originalSlice []string, removeString string) []string {
+	// go though the array
+	for i, content := range originalSlice {
+		// if the array matches with the string, you remove it from the array
+		if content == removeString {
+			return append(originalSlice[:i], originalSlice[i+1:]...)
+		}
+	}
+	return originalSlice
 }
