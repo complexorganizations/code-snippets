18578c55a1a639af7e3f4eab428d4f88f8d2d584
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-17 03:43:22 -0400

    Update write-to-file.rs

--- rust/write-to-file.rs
+++ rust/write-to-file.rs
@@ -2,7 +2,9 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
+    // the name of the file to write to
     let mut file = File::create("foo.txt")?;
+    // the content to write to the file
     file.write_all(b"Hello, world!")?;
     Ok(())
 }
