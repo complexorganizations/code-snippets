49a6cb501fab5c6460ba766fb363216d1eec687e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-17 03:42:08 -0400

    Update current-dir.rs

--- rust/current-dir.rs
+++ rust/current-dir.rs
@@ -1,7 +1,9 @@
 use std::env;
 
 fn main() -> std::io::Result<()> {
+    // Get the current working directory
     let path = env::current_dir()?;
+    // Print the current working directory
     println!("The current directory is {}", path.display());
     Ok(())
 }
