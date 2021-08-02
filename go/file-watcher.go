583279c4626f185ce97a0b1c8d6187823d6b76ef
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-02 12:22:58 -0400

    Update file-watcher.go

--- go/file-watcher.go
+++ go/file-watcher.go
@@ -2,65 +2,26 @@ package main
 
 import (
 	"crypto/sha512"
-	"flag"
-	//"fmt"
 	"io"
 	"log"
-	//"io/ioutil"
 	"os"
-	"path/filepath"
 )
 
 var (
-	watchSystemPath string
-	logSystemPath   string
+	watchSystemPath string = "test"
+	logSystemPath   string = "logs.log"
 )
 
-func init() {
-	if len(os.Args) > 1 {
-		tempWatchSystemPath := flag.String("watch", "/user/example/folder/file", "The system path to keep an eye on")
-		tempLogSystemPath := flag.String("log", "/log/file-watcher.log", "The logs system route")
-		flag.Parse()
-		watchSystemPath = *tempWatchSystemPath
-		logSystemPath = *tempLogSystemPath
-	} else {
-		os.Exit(0)
-	}
-	if len(watchSystemPath) < 1 || watchSystemPath == "/user/example/folder/file" {
-		log.Fatal("Error: The system path has not been given.")
-	}
-	if len(logSystemPath) > 1 {
-		//
-	}
-}
-
 func main() {
-	if fileExists(watchSystemPath) {
-		generateSHA512(watchSystemPath)
-	} else if folderExists(watchSystemPath) {
-		SHA512everything()
-	} else {
-		log.Fatal("Error: The machine direction is invalid.")
-	}
+	generateSHA512(watchSystemPath)
 }
 
 func generateSHA512(filePath string) {
-	file, err := os.Open(filePath)
-	handleErrors(err)
+	file, _ := os.Open(filePath)
 	defer file.Close()
 	hash := sha512.New()
 	io.Copy(hash, file)
-	handleErrors(err)
 	writeToFile(logSystemPath, hash.Sum(nil))
-}
-
-func SHA512everything() {
-	filepath.Walk(watchSystemPath, func(path string, info os.FileInfo, err error) error {
-		if fileExists(path) {
-			generateSHA512(path)
-		}
-		return nil
-	})
 }
 
 func writeToFile(filePath string, fileData []byte) {
@@ -70,26 +31,4 @@ func writeToFile(filePath string, fileData []byte) {
 	}
 	file.Write(fileData)
 	file.Close()
-}
-
-func folderExists(foldername string) bool {
-	info, err := os.Stat(foldername)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return info.IsDir()
-}
-
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-func handleErrors(err error) {
-	if err != nil {
-		log.Println(err)
-	}
 }
