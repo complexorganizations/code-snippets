3f7120b9929ae9f7b95bfa8f5ee1d730c51682dc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-03 09:05:12 -0400

    Create if-statemenet.dart

---
+++ dart/if-statemenet.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Declare the variables
+  var year = 1977;
+  var flybyObjects = ["Jupiter", "Saturn", "Uranus", "Neptune"];
+  // if statement
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+}
