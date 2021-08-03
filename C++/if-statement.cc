bb2cca43ae3707eec59992c7b221737d556d8ca7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-03 16:30:39 -0400

    Update if-statement.cc

--- C++/if-statement.cc
+++ C++/if-statement.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  if (20 > 18) {
+int main()
+{
+  if (20 > 18)
+  {
     cout << "20 is greater than 18";
-  } else {
+  }
+  else
+  {
     cout << "20 is not greater than 18";
   }
   return 0;
