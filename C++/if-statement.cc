6d45a2a9aa70051a1265c287c3a11a38dec7fa0a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-03 10:31:56 -0400

    Create if-statement.cc

---
+++ C++/if-statement.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  if (20 > 18) {
+    cout << "20 is greater than 18";
+  } else {
+    cout << "20 is not greater than 18";
+  }
+  return 0;
+}
