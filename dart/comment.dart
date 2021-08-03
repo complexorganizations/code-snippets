be9ef16e4e02c0bb127afa9e98ab33f459cb0ae2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-03 08:54:39 -0400

    Create comment.dart

---
+++ dart/comment.dart
@@ -0,0 +1,12 @@
+/*
+This is a multi line commment.
+Random Note two
+Random note three
+*/
+
+void main() {
+  print("Hello, World");
+}
+
+// This is a single line comment.
+// Another single line comment
