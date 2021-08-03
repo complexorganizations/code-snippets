bef33640769c0d57bf59789dedf254bfc3e7a472
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-03 10:51:51 -0400

    Create write-to-file.js

---
+++ JavaScript/write-to-file.js
@@ -0,0 +1,12 @@
+// Requiring fs module in which
+// writeFile function is defined.
+const fs = require("fs");
+
+// Data which will write in a file.
+let data = "Learning how to write in a file.";
+
+// Write data in 'Output.txt' .
+fs.writeFile("Output.txt", data, (err) => {
+  // In case of a error throw err.
+  if (err) throw err;
+});
