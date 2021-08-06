d311d8ba8688648a9901cd737e1767522b698c8e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-06 11:19:05 -0400

    Update read-file.cc

--- C++/read-file.cc
+++ C++/read-file.cc
@@ -1,14 +1,12 @@
-// Create a text string, which is used to output the text file
-string myText;
+#include <iostream>
+#include <fstream>
+#include <string>
 
-// Read from the text file
-ifstream MyReadFile("filename.txt");
-
-// Use a while loop together with the getline() function to read the file line by line
-while (getline (MyReadFile, myText)) {
-  // Output the text from the file
-  cout << myText;
+int main ()
+{
+  std::ifstream file("input.txt");
+  std::string str;
+  while (std::getline(file, str)) {
+    std::cout << str << "\n";
+  }
 }
-
-// Close the file
-MyReadFile.close();
