713da4b1850d8190c58aa0d109efa73a2ea51007
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-06 10:34:58 -0400

    Update random-string.go

--- Go/random-string.go
+++ Go/random-string.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println(randomString(1))
+	fmt.Println(randomString(64))
 }
 
 func randomString(bytesSize int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
 	randomBytes := make([]byte, bytesSize)
 	rand.Read(randomBytes)
 	randomString := fmt.Sprintf("%X", randomBytes)
