7e0b8189ea966390207e28f3b8350a93e1280fe0
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-10 15:26:35 -0400

    Create http-server.go

---
+++ Go/http-server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// The content to write to the response
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	// Route to the hello function
+	http.HandleFunc("/", hello)
+	// Listen and serve on port 8080.
+	err := http.ListenAndServe(":8080", nil)
+	// Return an error if something went wrong
+	if err != nil {
+		log.Println(err)
+	}
+}
