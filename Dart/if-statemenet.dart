a945e5ecff741e2e63165d57a30f024ec27dea53
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-10 16:41:22 -0400

    Update if-statemenet.dart

--- Dart/if-statemenet.dart
+++ Dart/if-statemenet.dart
@@ -1,26 +1,38 @@
 void main() {
-  // Declare the variables
+  // Declare all the variables
+  var name = "John Doe";
+  var age = 30;
   var year = 1977;
-  var flybyObjects = ["Jupiter", "Saturn", "Uranus", "Neptune"];
-  // if statement
-  if (year >= 2001) {
-    print('21st century');
-  } else if (year >= 1901) {
-    print('20th century');
+  var is_verified = true;
+  var friends = ["Jane Doe", "John Smith"];
+
+  // Print the variables
+  print(name);
+
+  // if statment
+  if (is_verified) {
+    print("Verified user");
+  }
+
+  if (age > 18) {
+    print("Eligible to vote");
+  } else if (age < 18) {
+    print("Not eligible to vote");
+  } else if (age == 30) {
+    print(age);
   } else {
-    print('19th century');
-  }
-  
-
-  for (var object in flybyObjects) {
-    print(object);
+    print("Invalid age.");
   }
 
+  // loop over a array.
+  for (var name in friends) {
+    print(name);
+  }
+
+  // if statement inside of a for block.
   for (int month = 1; month <= 12; month++) {
-    print(month);
-  }
-
-  while (year < 2016) {
-    year += 1;
+    if (month == 5) {
+      print(month);
+    }
   }
 }
