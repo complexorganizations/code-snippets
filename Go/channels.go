c90347a1eb8a00252bf095939476304b6dc50658
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-10 09:41:22 -0400

    Create channels.go

---
+++ Go/channels.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Prints a greeting message using values received in
+// the channel
+func greet(c chan string) {
+
+	name := <-c // receiving value from channel
+	fmt.Println("Hello", name)
+}
+
+func main() {
+	// Making a channel of value type string
+	c := make(chan string)
+
+	// Starting a concurrent goroutine
+	go greet(c)
+
+	// Sending values to the channel c
+	c <- "World"
+
+	// Closing channel
+	close(c)
+}
