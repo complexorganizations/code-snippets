3f98e00b596d088df756fbd27a15b26df410d45f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-11 21:43:39 -0400

    Update remove-duplicate-from-slice.go

--- Go/remove-duplicate-from-slice.go
+++ Go/remove-duplicate-from-slice.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// List of random strings.
@@ -23,14 +25,24 @@ func main() {
 	fmt.Println(uniqueNames)
 }
 
+// Make a unique array of strings.
 func makeUnique(randomStrings []string) []string {
-	flag := make(map[string]bool)
 	var uniqueString []string
-	for _, content := range randomStrings {
-		if !flag[content] {
-			flag[content] = true
-			uniqueString = append(uniqueString, content)
+	for _, value := range randomStrings {
+		if !arrayContains(value, uniqueString) {
+			uniqueString = append(uniqueString, value)
 		}
 	}
+	// return the array
 	return uniqueString
+}
+
+// Check if the array contains the value.
+func arrayContains(cointains string, originalArray []string) bool {
+	for _, value := range originalArray {
+		if value == cointains {
+			return true
+		}
+	}
+	return false
 }
