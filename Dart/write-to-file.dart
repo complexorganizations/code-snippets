7c36dfc7b3183dfdbcbec0b196b363eee5f93866
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-19 09:52:45 -0400

    Update write-to-file.dart

--- Dart/write-to-file.dart
+++ Dart/write-to-file.dart
@@ -1,5 +1,20 @@
 import "dart:io";
 
 void main() async {
-  File("file.txt").writeAsString("some content");
+  // No variables.
+  File("file.txt").writeAsString("This is the content to write to the file.");
+
+  // Variables.
+  String fileInSystem = "apple.txt";
+  String contentToWrite = "Some random string goes here";
+  File(fileInSystem).writeAsString(contentToWrite);
+
+  // Partial variables.
+  String systemPath = "rock.txt";
+  File(systemPath)
+      .writeAsString("This is a random string we will write to this file.");
+
+  // Partial variables with a default value.
+  String randomString = "This is a random string.";
+  File("dart.txt").writeAsString(randomString);
 }
