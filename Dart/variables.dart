eb76485e76dce5dbea66b36c9210b581a4e8ddc7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-19 09:47:31 -0400

    Update variables.dart

--- Dart/variables.dart
+++ Dart/variables.dart
@@ -1,17 +1,54 @@
+// Ways to declare a variable in dart.
 void main() {
-  // Declare the variables
-  var name = "Voyager I";
-  var year = 1977;
-  var antennaDiameter = 3.7;
-  var flybyObjects = ["Jupiter", "Saturn", "Uranus", "Neptune"];
-  var image = {
-    "tags": ["saturn"],
-    "url": "//path/to/saturn.jpg"
+  // 1. var <Name>;
+  var fullName = "John Doe";
+  print(fullName);
+  // 2. var <Name> = <Value>;
+  var age;
+  age = 30;
+  print(age);
+  // 3. <Type> <Name>;
+  int dob;
+  dob = 2000;
+  print(dob);
+  // 4. <Type> <Name> = <Value>;
+  String gender = "Male";
+  print(gender);
+
+  // More examples
+  var girlName = "Jane Doe";
+  print(girlName);
+
+  var dobYearForGirl;
+  dobYearForGirl = 2000.12;
+  print(dobYearForGirl);
+
+  // float values
+  double monthes;
+  monthes = 12.43;
+  print(monthes);
+
+  // Slice
+  var list = ["Apple", "Banana", "Orange", "Strawberry", "Mango", "Pineapple"];
+  print(list);
+  // From the slice get the first element
+  print(list[0]);
+  // Get the second element from the slice
+  print(list[1]);
+
+  // Bool
+  var isFemale = true;
+  print(isFemale);
+
+  // Maps
+  var content = {
+    "name": "John Doe",
+    "age": 30,
   };
-  // printing the variables
-  print(name);
-  print(year);
-  print(antennaDiameter);
-  print(flybyObjects);
-  print(image);
+  // Get the value of the age key
+  print(content);
+  print(content["age"]);
+  // Set the value of the age key
+  content["age"] = 31;
+  print(content["age"]);
 }
