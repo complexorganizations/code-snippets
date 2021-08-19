4199ea5c9e9e05df78e0dae7a6795d79115c23dd
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-19 11:17:12 -0400

    Update read-file.dart

--- Dart/read-file.dart
+++ Dart/read-file.dart
@@ -1,7 +1,23 @@
 import "dart:io";
 
+// The path of where the file is.
+var filePath = "file.txt";
+
 void main() {
-  File("file.txt").readAsString().then((String contents) {
+  // Read the file
+  File(filePath).readAsString().then((String contents) {
     print(contents);
+  });
+  // Test if the file has a certain thing.
+  testFunction();
+}
+
+void testFunction() {
+  File(filePath).readAsString().then((String contents) {
+    if (contents == "Hello, world!") {
+      print("Test passed");
+    } else {
+      print("Test failed");
+    }
   });
 }
