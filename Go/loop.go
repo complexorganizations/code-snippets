8004cced5c99336b6482ca64578c0abcd346c0e0
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-20 18:00:47 -0400

    Update loop.go

--- Go/loop.go
+++ Go/loop.go
@@ -13,6 +13,16 @@ func main() {
 	for loop := 0; loop < 5; loop++ {
 		fmt.Println("This is a loop, and it will go around 5 times")
 	}
+	// loop though a list of strings
+	names := []string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	for index, name := range names {
+		fmt.Println(index, name)
+	}
 	// loop forever
 	for {
 		fmt.Println("This is a loop and it will go on forever")
