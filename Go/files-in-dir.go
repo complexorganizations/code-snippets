a6e661d5b79846607c6729a5c0f90557a9eb95fb
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-20 15:13:57 -0400

    Update files-in-dir.go

--- Go/files-in-dir.go
+++ Go/files-in-dir.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println(filesInDirectory("test/"))
+	numberOfFiles, err := filesInDirectory("/")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(numberOfFiles)
 }
 
-func filesInDirectory(filePath string) int {
-	files, _ := os.ReadDir(filePath)
-	return len(files)
+func filesInDirectory(filePath string) (int, error) {
+	files, err := os.ReadDir(filePath)
+	if err != nil {
+		return 0, err
+	}
+	return len(files), err
 }
