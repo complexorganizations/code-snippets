ba21c2263c1b501f0000d51d8e82c29b7d4e8de3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-22 06:04:05 -0400

    Update cidr-range-contains.go

--- Go/cidr-range-contains.go
+++ Go/cidr-range-contains.go
@@ -4,28 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
-	check, err := checkIfIPInRange("10.8.0.1")
+	cidrRange := []string{
+		"10.0.0.0/8",
+		"fd12:3456:789a:1::/64",
+	}
+	check, err := isIPinCIDR(cidrRange, "127.0.0.1")
 	if err != nil {
 		log.Println(err)
 	}
 	fmt.Println(check)
 }
 
-func checkIfIPInRange(ip string) (bool, error) {
-	cidrRange := []string{
-		"10.0.0.0/8",
-		"fd12:3456:789a:1::/64",
-	}
-	for _, cidr := range cidrRange {
-		_, ipnet, err := net.ParseCIDR(cidr)
-		if err != nil {
-			return false, err
-		}
-		if ipnet.Contains(net.ParseIP(ip)) {
-			return true, nil
+// Check if a specific IP address is in a cidr.
+func isIPinCIDR(cidrSlice []string, networkIdentity string) (bool, error) {
+	for _, cidr := range cidrSlice {
+		if strings.Contains(cidr, "/") {
+			_, ipnet, err := net.ParseCIDR(cidr)
+			if err != nil {
+				return false, err
+			}
+			if ipnet.Contains(net.ParseIP(networkIdentity)) {
+				return true, nil
+			}
 		}
 	}
 	return false, nil
