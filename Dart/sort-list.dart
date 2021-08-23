da2353823492e4e750dad6984077f616bd5e6eda
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-22 23:55:10 -0400

    Create sort-list.dart

---
+++ Dart/sort-list.dart
@@ -0,0 +1,34 @@
+void main() {
+  // Sort a list
+  var randomStuff = [
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j"
+  ];
+  print(randomStuff); // without sorting the list
+  randomStuff.sort();
+  print(randomStuff); // while sorting the list
+}
