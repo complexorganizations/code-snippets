37b19deb2cbbbbc13a29a86095443cefd5970bd2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-22 23:36:02 -0400

    Update string-contains.dart

--- Dart/string-contains.dart
+++ Dart/string-contains.dart
@@ -1,5 +1,7 @@
 void main() {
+  // The string we will use for this example.
   String apple = "This is a random string";
+  // Returns either true or false.
   print(apple.contains("a")); // true
   print(apple.contains("b")); // false
 }
