25888c4599fb6e03a96c5c3b6d40d483fdf58d6c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-22 23:50:28 -0400

    Create basic-math.dart

---
+++ Dart/basic-math.dart
@@ -0,0 +1,39 @@
+void main() {
+  // We will add two numbers.
+  int a = 1;
+  int b = 2;
+  // The result will be stored in `c`.
+  int c = a + b;
+  // Prints `3`.
+  print(c);
+
+  // We can also use the `+` operator.
+  print(5 + 5);
+
+  // We can also use the `-` operator.
+  print(5 - 39);
+
+  // We can also use the `*` operator.
+  print(5 * 343);
+
+  // We can also use the `/` operator.
+  print(5 / 3);
+
+  // We can also use the `%` operator.
+  print(5 % 3);
+
+  // We can also use the `<<` operator.
+  print(5 << 2);
+
+  // We can also use the `>>` operator.
+  print(5 >> 2);
+
+  // We can also use the `&` operator.
+  print(5 & 3);
+
+  // We can also use the `|` operator.
+  print(5 | 3);
+
+  // We can also use the `^` operator.
+  print(5 ^ 3);
+}
