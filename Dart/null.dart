445e441a7db8526b040b0932a92b13854e421470
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-23 08:30:08 -0400

    Update null.dart

--- Dart/null.dart
+++ Dart/null.dart
@@ -1,18 +1,32 @@
 void main() {
+  // Make a null value.
   var stuffHere = null;
   print(stuffHere);
+  // a null value can be assigned to any type.
   stuffHere = "stuff goes here";
   print(stuffHere);
+  // a null value from string to a int
   stuffHere = 2;
   print(stuffHere);
+  // a null value to a double
   stuffHere = 2.5;
   print(stuffHere);
+  // a null value to a bool
+  stuffHere = true;
+  print(stuffHere);
+  // a null value to a list
+  stuffHere = [1, 2, 3];
+  print(stuffHere);
+  // a null value to a map
+  stuffHere = {'one': 1, 'two': 2, 'three': 3};
+  print(stuffHere);
+  // a null value to a null
   stuffHere = null;
   print(stuffHere);
-  
+  // a null int value
   int? aNullableInt = null;
   print(aNullableInt);
-  
+  // a null double value
   double? randomStuff = null;
   print(randomStuff);
 }
