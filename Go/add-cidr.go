3e472e147112c34b50b319691f8ac5a7db4ced54
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-23 09:10:26 -0400

    Update add-cidr.go

--- Go/add-cidr.go
+++ Go/add-cidr.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	fmt.Println(addCidr(net.ParseIP("1.1.1.1")))
+	fmt.Println(addCidr(net.ParseIP("8.8.8.8/32"))) // Already contains a cidr
 	fmt.Println(addCidr(net.ParseIP("2606:4700:4700::1111")))
+	fmt.Println(addCidr(net.ParseIP("2606:4700:4700::1111/128"))) // Already contains a cidr
 }
 
 // Add a cidr if no cidr is found
