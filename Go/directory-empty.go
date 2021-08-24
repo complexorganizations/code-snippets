266db9495429febf624ab9557a217ec1705d29ff
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-24 08:50:32 -0400

    Update directory-empty.go

--- Go/directory-empty.go
+++ Go/directory-empty.go
@@ -9,7 +9,10 @@ func main() {
 	fmt.Println(isDirEmpty("test/"))
 }
 
-func isDirEmpty(filePath string) bool {
-	files, _ := os.ReadDir(filePath)
-	return len(files) == 0
+func isDirEmpty(filePath string) (bool, error) {
+	files, err := os.ReadDir(filePath)
+	if err != nil {
+		return false, err
+	}
+	return len(files) == 0, nil
 }
