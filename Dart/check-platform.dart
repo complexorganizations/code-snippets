107a3f388cdfc555e7768a9540f8e4980f55069b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-28 21:34:28 -0400

    Create check-platform.dart

---
+++ Dart/check-platform.dart
@@ -0,0 +1,26 @@
+import "dart:io" show Platform;
+
+void main() {
+  switch (Platform.operatingSystem) {
+    case "android":
+      print("android");
+      break;
+    case "ios":
+      print("ios");
+      break;
+    case "linux":
+      print("linux");
+      break;
+    case "macos":
+      print("macos");
+      break;
+    case "windows":
+      print("windows");
+      break;
+    case "fuchsia":
+      print("fuchsia");
+      break;
+    default:
+      print("unknown");
+  }
+}
