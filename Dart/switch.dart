50ec5f1ded7ecfcaecb454309fbbe01f450f0b4d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-28 22:08:12 -0400

    Create switch.dart

---
+++ Dart/switch.dart
@@ -0,0 +1,13 @@
+void main() {
+  var restValue = 1;
+  switch (restValue) {
+    case 1:
+      print("one");
+      break;
+    case 2:
+      print("two");
+      break;
+    default:
+      print("other");
+  }
+}
