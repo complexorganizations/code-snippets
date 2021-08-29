0f318fc634e401394a1894ab63995622c3094502
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-28 22:23:08 -0400

    Update loop.dart

--- Dart/loop.dart
+++ Dart/loop.dart
@@ -1,9 +1,10 @@
 void main() {
+  // Go up in value
   for (var loop = 0; loop <= 100; loop++) {
     print(loop);
   }
+  // Go down in value
   for (var hole = 75; hole >= 50; hole--) {
     print(hole);
   }
 }
-
