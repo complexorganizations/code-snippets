ffcf2d9fc98a75cefca78828d2a220458cf897c2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-01 19:21:28 -0400

    Update function.go

--- Go/function.go
+++ Go/function.go
@@ -5,12 +5,19 @@ import (
 )
 
 func main() {
+	// Single return value
+	fmt.Println(addTwoNumber(1, 9))
 	// Multiple return values
 	fmt.Println(multipleReturnValues())
 	firstValue, _ := multipleReturnValues()
 	fmt.Println(firstValue)
 	_, secondValue := multipleReturnValues()
 	fmt.Println(secondValue)
+}
+
+// Single return value
+func addTwoNumber(firstNumber int, secondNumber int) int {
+	return firstNumber + secondNumber
 }
 
 // Multiple return values
