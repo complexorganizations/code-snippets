81f5e024a665c509f3711fb93c7d9ade8ac61c7b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-01 19:19:44 -0400

    Update os.go

--- Go/os.go
+++ Go/os.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
+	// Get the local system temp dir
 	fmt.Println(os.TempDir())
+	// Get the local system hostname
+	hostname, err := getSystemHostname()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(hostname)
+}
+
+func getSystemHostname() (string, error) {
+	systemHostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	return systemHostname, nil
 }
