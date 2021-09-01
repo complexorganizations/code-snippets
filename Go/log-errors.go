81f68ee4f0f585990af50b8d83f80a261316b64b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-01 08:32:28 -0400

    Update log-errors.go

--- Go/log-errors.go
+++ Go/log-errors.go
@@ -10,16 +10,13 @@ var err error
 func main() {
 	if exampleFunction("exmple-file") {
 		fmt.Println("It works")
-	} else {
-		fmt.Println("its broken")
 	}
 }
 
 func exampleFunction(filePath string) bool {
 	err = os.Remove(filePath)
 	if err != nil {
-		log.Println(err)
-		return false
+		log.Fatalln(err)
 	}
 	return true
 }
