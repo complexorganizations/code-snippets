abc2d352c6f828d5221fea517bf98826291ecd6b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-02 16:32:23 -0400

    Update rename-file.rs

--- Rust/rename-file.rs
+++ Rust/rename-file.rs
@@ -2,8 +2,8 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
     // the path for new and old files
-    let old_path = "./hello.txt";
-    let new_path = "./hello2.txt";
+    let old_path = "foo.txt";
+    let new_path = "bar.txt";
     // rename old file to new one
     fs::rename(old_path, new_path)?;
     Ok(())
