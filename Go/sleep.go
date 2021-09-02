f433d63d0b3ebaebd7a4f571e5cf3664eab196d7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-02 16:45:59 -0400

    Create sleep.go

---
+++ Go/sleep.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Do something in this area.
+	fmt.Println("Hello")
+	// For a specific amount of time, sleep.
+	time.Sleep(10000)
+	// After you've slept, do something different.
+	fmt.Println("World")
+}
