933a9240ef90d391812048573b48544d4b891ef0
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-02 16:11:57 -0400

    update

--- C++/variable.cpp
+++ C++/variable.cpp
@@ -2,34 +2,44 @@
 using namespace std;
 
 int main() {
-  // Integer
+  //
+}
+
+// String
+void stringFunction() {
+  string s = "Hello";
+  printf("%s\n", s.c_str());
+  string t = "World";
+  string u = s + t;
+  printf("%s\n", u.c_str());
+}
+
+// Integer
+void integerFunction() {
   int a = 10;
   int b = 20;
   int c = a + b;
   printf("%d\n", c);
-  // Float
-  float d = 10.0;
-  float e = 20.0;
-  float f = d + e;
-  printf("%f\n", f);
-  // String
-  string g = "Hello";
-  string h = "World";
-  string i = g + h;
-  printf("%s\n", i.c_str());
-  // Character
-  char j = 'a';
-  char k = 'b';
-  char l = j + k;
-  printf("%c\n", l);
-  // Boolean
-  bool m = true;
-  bool n = false;
-  bool o = m && n;
-  printf("%d\n", o);
-  // Array
-  int s[3] = {1, 2, 3};
-  int t[3] = {4, 5, 6};
-  int u[3] = {s[0] + t[0], s[1] + t[1], s[2] + t[2]};
-  printf("%d %d %d\n", u[0], u[1], u[2]);
+}
+
+// Boolean
+void booleanFunction() {
+  bool a = true;
+  printf("%d\n", a);
+}
+
+// Array
+void arrayFunction() {
+  int a[5] = {1, 2, 3, 4, 5};
+  printf("%d\n", a[0]);
+  printf("%d\n", a[1]);
+  printf("%d\n", a);
+}
+
+// Float
+void floatFunction() {
+  float a = 10.0;
+  float b = 20.0;
+  float c = a + b;
+  printf("%f\n", c);
 }
