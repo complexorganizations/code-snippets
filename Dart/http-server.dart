cb984ee190d35bdbdf581bfad249a0762d6b203d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-04 22:17:30 -0400

    Create http-server.dart

---
+++ Dart/http-server.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+main() async {
+  var server = await HttpServer.bind("localhost", 8080);
+
+  await for (var request in server) {
+    request.response.write("Hello world");
+    request.response.close();
+  }
+}
