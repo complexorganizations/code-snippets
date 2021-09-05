6467863e7f00187804b1951c7de363975f48c122
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-04 21:39:06 -0400

    Update loop.js

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -7,6 +7,20 @@ function main() {
     for (var i = 0; i < 100; i++) {
         console.log(i);
     }
+    // Break out of the loop
+    for (var i = 0; i < 10; i++) {
+        if (i > 5) {
+            break;
+        }
+        console.log(i);
+    }
+    // Continue to the next iteration
+    for (var i = 0; i < 10; i++) {
+        if (i > 5) {
+            continue;
+        }
+        console.log(i);
+    }
     // Loop Forever
     for (;;) {
         console.log("I will loop forever.");
