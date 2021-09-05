4a6333afa3075f48792ea5accc2346cce6f68c64
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-04 22:09:25 -0400

    Create http-client.py

---
+++ Python/http-client.py
@@ -0,0 +1,9 @@
+import http.client
+
+conn = http.client.HTTPSConnection("api.ipengine.dev")
+payload = ''
+headers = {}
+conn.request("GET", "/", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
