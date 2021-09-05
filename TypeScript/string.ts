a4ba00e9e5fffd0c9d494ecbb01786ee081d5769
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-04 22:01:18 -0400

    Update

---
+++ TypeScript/string.ts
@@ -0,0 +1,19 @@
+// Add two strings together
+function add(a, b) {
+    return a + b;
+}
+
+// Get the length of a string
+function getLength(s) {
+    return s.length;
+}
+
+// Get the first character of a string
+function first(s) {
+    return s[0];
+}
+
+// Get the last character of a string
+function last(s) {
+    return s[s.length - 1];
+}
