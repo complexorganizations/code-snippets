fca6ed9b9c1d8354f1d237c47862fe8da9661e22
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-05 07:44:01 -0400

    update

---
+++ Go/operators.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	// operators
+	fmt.Println(1 == 1) // Is equal
+	fmt.Println(1 != 1) // Is not equal
+	fmt.Println(1 < 1)  // Is less than
+	fmt.Println(1 > 1)  // Is greater than
+	fmt.Println(1 <= 1) // Is less than or equal to
+	fmt.Println(1 >= 1) // Is greater than or equal to
+}
