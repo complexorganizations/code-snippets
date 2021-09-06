4baa16dc6eba438fe2a0dac1a87b61c00e7c6d2c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-06 17:19:37 -0500

    Update http-client.dart

--- Dart/http-client.dart
+++ Dart/http-client.dart
@@ -2,16 +2,9 @@ import "dart:io";
 import "dart:convert";
 
 void main() {
+  // Send a request to the http server and return the response.
   HttpClient()
       .getUrl(Uri.parse("https://www.example.com"))
-      .then((request) => request.close())
-      .then((response) => response.transform(Utf8Decoder()).listen(print));
-}
-
-// Send a request to the server and return the response.
-String? sendRequest(String url) {
-  HttpClient client = new HttpClient();
-  client.getUrl(Uri.parse(url))
       .then((request) => request.close())
       .then((response) => response.transform(Utf8Decoder()).listen(print));
 }
