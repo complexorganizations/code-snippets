2bc40dfde77714d875ddd543ca4627e2caabf616
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-06 16:32:37 -0500

    Update terminal.dart

--- Dart/terminal.dart
+++ Dart/terminal.dart
@@ -1,8 +1,21 @@
 import "dart:io";
 
 void main() {
+  // User input
+  userInput();
+  // User output
+  writeToTerminal();
+}
+
+
+// Take in user input and print it back to the user
+void userInput() {
+  print("Enter your name");
+  var name = stdin.readLineSync();
+  print("Hello, ${name}");
+}
+
+// Write some text to the terminal
+void writeToTerminal() {
   stdout.writeln("Hello, World");
-  print("Whats your input");
-  var content = stdin.readLineSync();
-  print(content);
 }
