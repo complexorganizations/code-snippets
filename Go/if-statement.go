47a5c530130db64bb4b6ee72a266907621118653
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-10 22:43:14 -0400

    Update if-statement.go

--- Go/if-statement.go
+++ Go/if-statement.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func main() {
 	content := "test"
+	// if statement
+	if content == "test" {
+		fmt.Println(true)
+	}
+	// if / else
+	if content == "test" {
+		fmt.Println(true)
+	} else {
+		fmt.Println(false)
+	}
+	// if / else if
+	if content == "test" {
+		fmt.Println(true)
+	} else if content == "not-test" {
+		fmt.Println(false)
+	}
+	// if / else if / else
 	if content == "test" {
 		fmt.Println(true)
 	} else if content == "not-test" {
