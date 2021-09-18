9bde6c81b1521b160252ad1a6e7f40580381992f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-18 15:46:08 -0400

    Update url.go

--- Go/url.go
+++ Go/url.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+"log" 
 	"net/url"
 )
 
 func main() {
 	if validURL("https://www.example.com") {
 		fmt.Println("works")
+	}
+u, err := url.Parse("https://example.org:8000/path")
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
