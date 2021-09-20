fa82ecf472dffc17ab887186f37668d15438a700
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-20 13:04:30 -0400

    update

--- JavaScript/write.js
+++ JavaScript/write.js
@@ -1,15 +1,29 @@
+const fs = require("fs")
+
 // Main function
 function main() {
+  // Write to a file
   writeToFile("write-to-file.txt", "Hello World!")
+  // Append to a file
+  appendToFile("write-to-file.txt", "Hello World!")
 }
 
 main()
 
-// Write content to a file
+// Write some content to a specific file
 function writeToFile(fileName, content) {
   fs.writeFile(fileName, content, function (err) {
     if (err) {
-      return console.log(err)
+      return err
+    }
+  })
+}
+
+// Append and write to a file
+function appendToFile(fileName, content) {
+  fs.appendFile(fileName, content, function (err) {
+    if (err) {
+      return err
     }
   })
 }
