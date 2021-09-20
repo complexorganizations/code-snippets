4b6218cd44dae6ffb0e3d0e2c958da7125811b09
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-19 21:43:29 -0400

    Update operators.dart

--- Dart/operators.dart
+++ Dart/operators.dart
@@ -1,1 +1,19 @@
-
+void main() {
+  var fistName = "John";
+  var lastName = "Doe";
+  // && operator is used to check if multiple value is true or false.
+  if (fistName == "John" && lastName == "Doe") {
+    print("Hello ${fistName} ${lastName}");
+  }
+  // || operator is used to check if one of the value is true or false.
+  if (fistName == "Jane" || lastName == "Doe") {
+    print("Hello ${fistName} ${lastName}");
+  }
+  // ! operator is used to reverse the value.
+  if (fistName != "John") {
+    print("${fistName}");
+  }
+  if (!(fistName == "John" && lastName == "Doe")) {
+    print("Hello ${fistName} ${lastName}");
+  }
+}
