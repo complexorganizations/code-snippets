bc6bdab2219ac4eb3aff18fbdc998e0d879fd6b2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-19 22:23:37 -0400

    Update json.go

--- Go/json.go
+++ Go/json.go
@@ -19,38 +19,11 @@ func main() {
 		Name:   "Reds",
 		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
 	}
-	b, err := json.Marshal(group)
+	currentJson, err := json.Marshal(group)
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(string(b))
-	// Decode JSON data
-	var jsonContent = []byte(`{
-		"Name": "John Doe",
-		"Age": "25",
-		"Address": "123 Main Street",
-		"City": "New York",
-		"State": "NY",
-		"Zip": "10001",
-		"Phone": "123-456-7890",
-		"Email": "johnDoe@example.com"
-	}`)
-	type JsonStuff struct {
-		Name    string `json:"Name"`
-		Age     string `json:"Age"`
-		Address string `json:"Address"`
-		City    string `json:"City"`
-		State   string `json:"State"`
-		Zip     string `json:"Zip"`
-		Phone   string `json:"Phone"`
-		Email   string `json:"Email"`
-	}
-	var content []JsonStuff
-	err = json.Unmarshal(jsonContent, &content)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(content)
+	fmt.Println(string(currentJson))
 	// Check if a json file is valid.
 	validCheck, err := validateJsonFromFile("test.json")
 	if err != nil {
@@ -59,6 +32,7 @@ func main() {
 	fmt.Println(validCheck)
 }
 
+// Open a file and check if a json is valid.
 func validateJsonFromFile(jsonFile string) (bool, error) {
 	data, err := os.ReadFile(jsonFile)
 	if err != nil {
