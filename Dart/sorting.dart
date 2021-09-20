548512c7d0187359f725eb316b2ecce0e4c61b14
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-19 21:41:41 -0400

    Update sorting.dart

--- Dart/sorting.dart
+++ Dart/sorting.dart
@@ -1,1 +1,12 @@
+void main() {
+  var list = [3, 1, 2, 4, 5];
+  print(list);
+  var newList = sortIntList(list);
+  print(newList);
+}
 
+// Sort the list and return it.
+List<int> sortIntList(List<int> content) {
+  content.sort();
+  return content;
+}
