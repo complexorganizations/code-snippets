0ccefdebe6111387c78ffc359028f4358607bcde
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-19 21:08:55 -0400

    Update map.js

--- JavaScript/map.js
+++ JavaScript/map.js
@@ -1,22 +1,32 @@
-// Create a map of value.
-const map1 = new Map()
+function main() {
+    // Create a new map.
+    var newMap = new Map()
+    // Set some value to the map.
+    newMap.set("Name", "John")
+    newMap.set("Age", "30")
+    newMap.set("Address", "123 Main St")
+    newMap.set("Zip", "12345")
+    newMap.set("City", "New York")
+    newMap.set("State", "NY")
+    newMap.set("Country", "USA")
+    newMap.set("Phone", "123-456-7890")
+    newMap.set("Email", "johndoe@example.com")
+    // Print the whole map.
+    console.log(newMap)
+    // Get the value of the map.
+    var name = newMap.get("Name")
+    console.log(name)
+    // Remove the value of the map.
+    newMap.delete("Name")
+    console.log(name)
+    // Get the size of the map.
+    var size = newMap.size
+    console.log(size)
+    // Check if the map contains a key.
+    var contains = newMap.has("Name")
+    console.log(contains)
+    // Clear the map.
+    newMap.clear()
+}
 
-map1.set("a", 1)
-map1.set("b", 2)
-map1.set("c", 3)
-
-console.log(map1.get("a"))
-// expected output: 1
-
-map1.set("a", 97)
-
-console.log(map1.get("a"))
-// expected output: 97
-
-console.log(map1.size)
-// expected output: 3
-
-map1.delete("b")
-
-console.log(map1.size)
-// expected output: 2
+main()
