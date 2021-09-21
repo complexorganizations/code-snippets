a5e6734b35a68ac5d743cd598266f3c30001ce63
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-20 22:05:01 -0400

    Update directory.py

--- Python/directory.py
+++ Python/directory.py
@@ -1,13 +1,22 @@
 import os
 
 def main():
-    print(check_dir("Python"))
+    print(check_dir("/src/"))
 
 main()
 
 # Check if a directory exists
 def check_dir(dir_name):
-    if os.path.exists(dir_name):
-        return True
-    else:
-        return False
+    return os.path.exists(dir_name)
+
+# Create a directory
+def create_dir(dir_name):
+    os.mkdir(dir_name)
+
+# Remove a directory
+def remove_dir(dir_name):
+    os.rmdir(dir_name)
+
+# Change the current working directory
+def change_dir(dir_name):
+    os.chdir(dir_name)
