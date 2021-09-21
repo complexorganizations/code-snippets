b6d78b790f5df5cddda93ad221280d2b4419382a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-20 22:21:35 -0400

    Create function.py

---
+++ Python/function.py
@@ -0,0 +1,52 @@
+def main():
+    # Sum of two numbers
+    print(add(2, 3))
+    # Product of two numbers
+    print(multiply(2, 3))
+    # Quotient of two numbers
+    print(divide(2, 3))
+    # Difference of two numbers
+    print(subtract(2, 3))
+    # Remainder of two numbers
+    print(modulo(2, 3))
+    # Quotient of two numbers
+    print(quotient(2, 3))
+    # Power of two numbers
+    print(power(2, 3))
+    # Square of a number
+    print(square(2))
+
+# Call the main function
+main()
+
+# Create a function that takes two arguments and returns their sum
+def add(a, b):
+    return a + b
+
+# Create a function that takes two arguments and returns their product
+def multiply(a, b):
+    return a * b
+
+# Create a function that takes two arguments and returns their quotient
+def divide(a, b):
+    return a / b
+
+# Create a function that takes two arguments and returns their difference
+def subtract(a, b):
+    return a - b
+
+# Create a function that takes two arguments and returns their remainder
+def modulo(a, b):
+    return a % b
+
+# Create a function that takes two arguments and returns their quotient
+def quotient(a, b):
+    return a // b
+
+# Create a function that takes two arguments and returns their quotient
+def power(a, b):
+    return a ** b
+
+# Create a function that takes two arguments and returns their quotient
+def square(a):
+    return a ** 2
