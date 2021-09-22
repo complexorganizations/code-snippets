90518b6308aad662bfb72e419e589da5d4e79eb6
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-22 13:19:09 -0400

    update

--- JavaScript/if-statement.js
+++ JavaScript/if-statement.js
@@ -10,7 +10,10 @@ function main() {
     }
     // Test a if statement inside a function
     console.log(testIfStatement("test"))
-    console.log(testIfStatement("news"))
+    // Test a if else statement inside a function
+    console.log(testIfElseStatement("a"))
+    // Test a if, else if, else statement
+    console.log(testElseIfStatement("test"))
 }
 
 main()
@@ -18,6 +21,24 @@ main()
 // If statement inside a function
 function testIfStatement(content) {
     if (content == "test") {
+        return true
+    }
+}
+
+// If else statement inside a function
+function testIfElseStatement(content) {
+    if (content == "test") {
+        return true
+    } else {
+        return false
+    }
+}
+
+// if, else if, else statement
+function testElseIfStatement(content) {
+    if (content == "test") {
+        return true
+    } else if (content == "test2") {
         return true
     } else {
         return false
