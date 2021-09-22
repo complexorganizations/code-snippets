05e3d860b87963c3c5bb52b320b7e1b5595937c2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-22 18:50:38 -0400

    Update if-statement.rs

--- Rust/if-statement.rs
+++ Rust/if-statement.rs
@@ -1,10 +1,35 @@
 fn main() {
-    let n = 5;
-    if n < 0 {
-        print!("{} is negative", n);
-    } else if n > 0 {
-        print!("{} is positive", n);
+    if_statement();
+    if_else_if_statement();
+    if_else_if_else_statement();
+}
+
+// Example of a if statement
+fn if_statement() {
+    let food = "Apple";
+    if food == "Apple" {
+        println!("{}", food);
+    }
+}
+
+// Example of a if else if statement
+fn if_else_if_statement() {
+    let name = "John";
+    if name == "John" {
+        println!("{}", name);
+    } else if name == "Jane" {
+        println!("{}", name);
+    }
+}
+
+// Example of a if else if else statement
+fn if_else_if_else_statement() {
+    let age = 30;
+    if age == 10 {
+        println!("{}", age)
+    } else if age == 20 {
+        println!("{}", age)
     } else {
-        print!("{} is zero", n);
+        println!("Unknown")
     }
 }
