9efcdd651d7c0578d5efc8b8f8a6dcf9e209973c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-22 19:48:43 -0400

    Update function.rs

--- Rust/function.rs
+++ Rust/function.rs
@@ -1,13 +1,37 @@
-void main() {
-    //
+fn main() {
+    // Example function
+    example_function();
+    // Takes in a single argument and returns an integer
+    let a = 1;
+    let b = add_one(a);
+    println!("{}", b);
+    // Takes in two int arguments and returns an int
+    let c = 3;
+    let d = 4;
+    let e = add_numbers(c, d);
+    println!("{}", e);
+    // Takes in two int arguments and returns two ints
+    let (f, g) = (5, 6);
+    let (h, j) = swap(f, g);
+    println!("{} {}", h, j);
 }
 
-// Single return value
+// Function that takes no arguments and returns nothing
+fn example_function() {
+    println!("I'm an example function!");
+}
+
+// Takes in a single argument and returns an integer
 fn add_one(x: i32) -> i32 {
     x + 1
 }
 
-// Multiple return values
+// Takes in two arguments and returns one integer
+fn add_numbers(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// Takes in two arguments and returns two integers
 fn swap(x: i32, y: i32) -> (i32, i32) {
     (y, x)
 }
