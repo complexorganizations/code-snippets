afd083e9de9f81cabdf367c7a36083c668850a0b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-22 11:06:15 -0400

    Update switch.py

--- Python/switch.py
+++ Python/switch.py
@@ -1,24 +1,31 @@
 # Example of a switch statment
 def switch_function(argument):
-    switcher = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
-    }
-    print(switcher.get(argument))
+    match argument:
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        case 3:
+            print("3")
 
+# A simple function example
+def simple_function():
+    print("This is a simple function")
+
+# Another switch function
+def another_switch(argument):
+    match argument:
+        case 1:
+            simple_function()
+        case 2:
+            print("2")
+        case 3:
+            print("3")
 
 def main():
     # Example of a switch statment
     switch_function(1)
+    # Another switch function
+    another_switch(2)
 
 main()
