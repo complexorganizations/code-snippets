db966dd411232c815b297269a1f80b8cbdaeb138
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-22 20:06:38 -0400

    Update if-statement.sh

--- ShellScript/if-statement.sh
+++ ShellScript/if-statement.sh
@@ -1,29 +1,48 @@
-VALUE="1"
+#!/usr/bin/env bash
 
-# if statement
-if [ "${VALUE}" == "1" ]; then
-  echo true
-fi
+# Global Variables
+VALUE=1
 
-# if / else statement
-if [ "${VALUE}" == "1" ]; then
-  echo true
-else
-  echo false
-fi
+# Example of a if statement
+function if_statement() {
+  if [ "${VALUE}" == 1 ]; then
+    echo true
+  fi
+}
 
-# if / elif statement
-if [ "${VALUE}" == "1" ]; then
-  echo true
-else if [ "${VALUE}" == "2" ]; then
-  echo false
-fi
+if_statement
 
-# if / elif / else
-if [ "${VALUE}" == "1" ]; then
-  echo true
-else if [ "${VALUE}" == "2" ]; then
-  echo false
-else [ "${VALUE}" == "1" ]; then
-  echo false
-fi
+# Example of a if else statement
+function if_else_statement() {
+  if [ "${VALUE}" == 1 ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+if_else_statement
+
+# Example of a if else if statement
+function if_elif_statement() {
+  if [ "${VALUE}" == 1 ]; then
+    echo true
+  elif [ "${VALUE}" == 2 ]; then
+    echo false
+  fi
+}
+
+if_elif_statement
+
+# Example of a if elif else statement
+function if_elif_else_statement() {
+  if [ "${VALUE}" == 1 ]; then
+    echo true
+  elif [ "${VALUE}" == 2 ]; then
+    echo false
+  else
+    echo unknown
+  fi
+}
+
+if_elif_else_statement
