d71313bc4dcc7bf799f51c1d4945d7ce786cebea
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-23 08:47:09 -0400

    Update variables.ts

--- TypeScript/variables.ts
+++ TypeScript/variables.ts
@@ -1,6 +1,12 @@
 function main() {
     // Variable
     createVariable();
+    // Map variable
+    createMapVariable();
+    // Integer variable
+    createIntegerVariable();
+    // Boolean variable
+    createBooleanVariable();
 }
 
 main()
@@ -11,13 +17,34 @@ function createVariable() {
     var x;
     // Variable initialization
     x = 10;
-    console.log(x);
     // Variable reassignment
     x = 20;
-    console.log(x);
     // Variable destruction
     var y = x;
     x = 30;
     console.log(x);
+    console.log(y);
+}
+
+// Create a map variable
+function createMapVariable() {
+    var x = {
+        name: "John",
+        age: 30
+    };
+    console.log(x);
+}
+
+// Create a interger variable
+function createIntegerVariable() {
+    var x = 10;
+    console.log(x);
+}
+
+// Create a boolean variable
+function createBooleanVariable() {
+    var x = true;
+    console.log(x);
+    var y = false;
     console.log(y);
 }
