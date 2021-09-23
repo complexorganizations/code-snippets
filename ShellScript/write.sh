85531e36e990de6395aa0c2da800290095c084b1
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-22 20:25:09 -0400

    Update write.sh

--- ShellScript/write.sh
+++ ShellScript/write.sh
@@ -2,14 +2,28 @@
 
 # Append and write to file
 function append-and-write() {
+    echo "${1}" >>foo.txt
+}
+
+append-and-write "Hello World"
+
+# Write some text to a file with appending to it
+function write-to-file-with-append() {
     echo "some string here" >>foo.txt
 }
 
-append-and-write
+write-to-file-with-append
 
 # Don't append and write to file
 function dont-append-and-write() {
-    echo "some string here" >foo.txt
+    echo "${1}" >bar.txt
 }
 
-dont-append-and-write
+dont-append-and-write "Hello World"
+
+# Write some text to a file while not appending to it.
+function write-to-file-without-append() {
+    echo "some stuff here" >bar.txt
+}
+
+write-to-file-without-append
