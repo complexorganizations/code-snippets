a859d704ffef910e4e871409d30e54936e6d8c14
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 07:44:23 -0400

    Update if-statement.h

--- C/if-statement.h
+++ C/if-statement.h
@@ -7,6 +7,8 @@ int main() {
     if_else_statement();
     // if-else-if-statement
     if_else_if_statement();
+    // if-else-if-else-statement
+    if_else_if_else_statement();
     return 0;
 }
 
@@ -30,6 +32,16 @@ void if_else_statement() {
 
 // A simple if statement with an else if
 void if_else_if_statement() {
+    int a = 1;
+    if (a == 1) {
+        printf("a is 1\n");
+    } else if (a == 2) {
+        printf("a is 2\n");
+    }
+}
+
+// A simple if statement with an else if and else
+void if_else_if_else_statement() {
     int a = 1;
     if (a == 1) {
         printf("a is 1\n");
