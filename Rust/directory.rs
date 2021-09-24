26e4953ea33610a813e55d271b10b3d5c7912197
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 08:45:42 -0400

    Update directory.rs

--- Rust/directory.rs
+++ Rust/directory.rs
@@ -5,6 +5,12 @@ fn main() {
     println!("Current directory: {}", get_cwd());
     // Remove a directory
     remove_dir("/some/random/dir/");
+    // Check if a directory exists
+    if dir_exists("/some/random/dir/") {
+        println!("Directory exists!");
+    }
+    // Create a directory
+    create_dir("/some/random/dir/");
 }
 
 // Get the current working directory and return it as a String
@@ -16,4 +22,14 @@ fn get_cwd() -> String {
 // Remove the directory
 fn remove_dir(path: &str) {
     fs::remove_dir(path)?;
+}
+
+// Check if the directory exists
+fn dir_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+// Create a directory
+fn create_dir(path: &str) {
+    fs::create_dir(path)?;
 }
