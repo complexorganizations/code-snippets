b4264113d6cf623d7ea0affa177b2053d8be0308
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 06:30:49 -0400

    Update parse.dart

--- Dart/parse.dart
+++ Dart/parse.dart
@@ -1,3 +1,18 @@
 void main() {
-print("Hello World")
+  // Parse a int and return it.
+  int i = int.parse("FF", radix: 16);
+  print(i);
+  // Parse a double and return it.
+  double d = double.parse("-1.e3");
+  print(d);
+}
+
+// Parse a int and return it
+int parseInt(String s) {
+  return int.parse(s);
+}
+
+// Parse a double and return it
+double parseDouble(String s) {
+  return double.parse(s);
 }
