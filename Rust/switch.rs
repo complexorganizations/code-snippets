daf2566299e2c8d9a95163dc585eb90c2d85979b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 07:58:39 -0400

    Update switch.rs

--- Rust/switch.rs
+++ Rust/switch.rs
@@ -1,15 +1,38 @@
 fn main() {
-    // A simple switch statement
-    simple_switch();
+    // A simple switch statement with integers.
+    integer_switch();
+    // Switch statements can also be used with strings.
+    string_switch();
+    // Switch with boolean expressions.
+    boolean_switch();
 }
 
-// A simple switch statement
-fn simple_switch() {
+// Switch statement with integer
+fn integer_switch() {
     let x = 1;
     match x {
         1 => println!("one"),
         2 => println!("two"),
         3 => println!("three"),
         _ => println!("anything"),
+    }
+}
+
+// Switch statement with string
+fn string_switch() {
+    let x = "John Doe";
+    match x {
+        "Jane Doe" => println!("Hi Jane!"),
+        "John Doe" => println!("Hi John!"),
+        _ => println!("Hi!"),
+    }
+}
+
+// Switch statement with boolean
+fn boolean_switch() {
+    let x = true;
+    match x {
+        true => println!("true"),
+        false => println!("false"),
     }
 }
