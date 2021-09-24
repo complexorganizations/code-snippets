c720175b7768bce87ad8cf4b39e491d0a6907d20
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 11:07:39 -0400

    Update system_information.py

--- Python/system_information.py
+++ Python/system_information.py
@@ -1,6 +1,8 @@
 import os
 import platform
 import socket
+import http.client
+import re
 
 
 # Get the current operating system
@@ -50,6 +52,41 @@ def get_ip_address():
     return ip_address
 
 
+def get_response(hostname, path, payload, headers, method, encoding):
+    conn = http.client.HTTPSConnection(hostname)
+    conn.request(method, path, payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    returnData = data.decode(encoding)
+    return returnData
+
+
+# Get the current user external ipv4 address and return it
+def get_external_ipv4():
+    hostname = "api.ipengine.dev"
+    path = "/"
+    method = "GET"
+    payload = ""
+    encoding = "utf-8"
+    headers = {}
+    httpResponse = get_response(
+        hostname, path, payload, headers, method, encoding)
+    return re.search("\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", httpResponse)
+
+
+# Get the current user external ipv6 address
+def get_external_ipv6():
+    hostname = "api.ipengine.dev"
+    path = "/"
+    method = "GET"
+    payload = ""
+    encoding = "utf-8"
+    headers = {}
+    httpResponse = get_response(
+        hostname, path, payload, headers, method, encoding)
+    return re.search("\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\b", httpResponse)
+
+
 def main():
     # Get the current operating system
     print(get_os())
@@ -69,6 +106,10 @@ def main():
     print(get_machine_type())
     # Get the current user ip address and hostname of the system
     print(get_ip_address())
+    # Get the current user external ipv4 address
+    print(get_external_ipv4())
+    # Get the current user external ipv6 address
+    print(get_external_ipv6())
 
 
 main()
