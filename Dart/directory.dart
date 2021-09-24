c949d05f594ff79182e724cb4573f646eb4c5ee9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 05:57:41 -0400

    Update directory.dart

--- Dart/directory.dart
+++ Dart/directory.dart
@@ -3,9 +3,27 @@ import "dart:io";
 void main() {
   print(checkDirExists("/")); // true
   print(checkDirExists("/random/path/dosent/exists/")); // false
+  print(getFiles("/"));
+  print(getDirectories("/"));
 }
 
 // Check if a directory exists
 bool checkDirExists(String path) {
   return FileSystemEntity.typeSync(path) != FileSystemEntityType.notFound;
+}
+
+// Get all the files only in a directory
+List<File>? getFiles(String path) {
+  return Directory(path)
+      .listSync(recursive: false, followLinks: false)
+      .whereType<File>()
+      .toList();
+}
+
+// Get all the directories only in a directory
+List<Directory>? getDirectories(String path) {
+  return Directory(path)
+      .listSync(recursive: false, followLinks: false)
+      .whereType<Directory>()
+      .toList();
 }
