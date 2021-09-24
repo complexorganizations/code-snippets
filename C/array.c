a1ae1cf9954536181464e0dd89b5e2a16849f108
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 19:54:59 -0400

    Update array.c

--- C/array.c
+++ C/array.c
@@ -4,10 +4,14 @@
 int main() {
     // Create an array of integers
     int randomArray[5] = {19, 10, 8, 17, 9};
+    // Get the length of the array
+    int lengthOfArray = getArrayLength(randomArray);
+    printf("%d\n", lengthOfArray);
     // Get a random value from the array
     int randomItem = randomElement(randomArray, 5);
-    // Print the random value
-    printf("The random value is %d\n", randomItem);
+    printf("%d\n", randomItem);
+    // Remove all the values from the array
+    clearArray(randomArray, 5);
     return 0;
 }
 
@@ -15,4 +19,17 @@ int main() {
 int randomElement(int array[], int size) {
     int randomIndex = rand() % size;
     return array[randomIndex];
+}
+
+// Get the length of the array
+int getArrayLength(int array[]) {
+    int arrSize = sizeof(array) / sizeof(array[0]);
+    return arrSize;
+}
+
+// Remove all elements from the array
+void clearArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = 0;
+    }
 }
