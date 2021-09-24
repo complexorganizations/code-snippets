8819edfdf48c65dc72d93b867a93e98f7fe53c5d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 06:07:40 -0400

    Update exit.dart

--- Dart/exit.dart
+++ Dart/exit.dart
@@ -1,8 +1,15 @@
 import "dart:io";
 
 void main() {
-  print("This is a test");
-  exit(1);
-  // ignore: dead_code
+  // Some actions.
+  print("Hello, World!");
+  // Exit the program.
+  closeApplication(1);
+  // We will never get to this code.
   print("We will never get to this string.");
+}
+
+// Simple exit function
+void closeApplication(int code) {
+  exit(code);
 }
