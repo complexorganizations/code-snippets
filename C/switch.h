3dd37f5b896a5e6e10569521f80fbfa62ed2ecb9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 07:38:29 -0400

    Create switch.h

---
+++ C/switch.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    switch (a) {
+        case 0:
+            printf("0");
+            break;
+        case 1:
+            printf("1");
+            break;
+        case 2:
+            printf("2");
+            break;
+        default:
+            printf("default");
+    }
+}
