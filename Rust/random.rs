3e62faf4f6cc6d2f8fc2e2393e51c78ca389c4ff
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 07:53:50 -0400

    Update random.rs

--- Rust/random.rs
+++ Rust/random.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // Random number between 1 and 10
+    println!("{}", random_number(1, 10));
+}
+
+// Generate a random number between two numbers
+fn random_number(min: u32, max: u32) -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min, max)
 }
