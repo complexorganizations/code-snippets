4729bbfd41658b7d79944e6b20f7edec2e8008b7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 20:37:12 -0400

    Update switch.c

--- C/switch.c
+++ C/switch.c
@@ -1,19 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int a = 0;
-    switch (a) {
-        case 0:
-            printf("0");
-            break;
-        case 1:
-            printf("1");
-            break;
-        case 2:
-            printf("2");
-            break;
-        default:
-            printf("default");
-    }
-    return 0;
+  // A simple switch statement with ints
+  integerSwitch();
+  // A simple switch statement with strings
+  stringSwitch();
+  return 0;
+}
+
+// A simple integer switch statement
+void integerSwitch() {
+  int age = 25;
+  switch (age) {
+  case 25:
+    printf("25");
+    break;
+  case 26:
+    printf("26");
+    break;
+  case 27:
+    printf("27");
+    break;
+  default:
+    printf("default");
+  }
+}
+
+// A simple string switch statement
+void stringSwitch() {
+  char name = 'John';
+  switch (name) {
+  case 'John':
+    printf("John");
+    break;
+  case 'Jane':
+    printf("Jane");
+    break;
+  case 'Joe':
+    printf("Joe");
+    break;
+  default:
+    printf("default");
+  }
 }
