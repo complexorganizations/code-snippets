24e372d4129a59dd531a34d3d7c111e05e77b317
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 22:17:58 -0400

    Update array.py

--- Python/array.py
+++ Python/array.py
@@ -1,3 +1,6 @@
+import random
+
+
 # Check if the array is empty
 def is_array_empty(array):
     if len(array) == 0:
@@ -28,20 +31,57 @@ def get_array_index(array, element):
 def remove_duplicates(array):
     return list(set(array))
 
+
 # Remove the element from the array
-
-
 def remove_element(array, element):
     array.remove(element)
     return array
 
+
 # Add an element to the array
-
-
 def add_element(array, element):
     array.append(element)
     return array
 
+
+# Get a random element from the array
+def get_random_element(array):
+    return array[random.randint(0, len(array) - 1)]
+
+
+# Remove all the elements from the array
+def remove_all_elements(array):
+    array.clear()
+    return array
+
+
+# Suffle the array
+def shuffle_array(array):
+    random.shuffle(array)
+    return array
+
+
+
+# Get the length of the array
+def get_array_length(array):
+    return len(array)
+
+
+# Reverse the array
+def reverse_array(array):
+    return array[::-1]
+
+
+# Change the value of a element at the certain index
+def change_element(array, index, element):
+    array[index] = element
+    return array
+
+
+# Change the value of a element in the array to a certain value
+def change_element(array, element, value):
+    array[array.index(element)] = value
+    return array
 
 def main():
     array = ["a", "b", "c"]
@@ -59,6 +99,21 @@ def main():
     print(remove_element(array, "a"))
     # Add an element to the array
     print(add_element(array, "d"))
+    # Get a random element from the array
+    print(get_random_element(array))
+    # Remove all the elements from the array
+    print(remove_all_elements(array))
+    # Suffle the array
+    print(shuffle_array(array))
+    # Get the length of the array
+    print(get_array_length(array))
+    # Reverse the array
+    print(reverse_array(array))
+    # Change the value of a element at the certain index
+    print(change_element(array, 1, "e"))
+    # Change the value of a element in the array
+    print(change_element(array, "a", "f"))
+
 
 
 main()
