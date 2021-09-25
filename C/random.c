15df9b5ab44d5253ccc0315ee36c5527460f2881
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-25 08:04:19 -0400

    Update random.c

--- C/random.c
+++ C/random.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("%d", randomNumberInRange(2, 5));
-}
-
 // Generate a random number between x and y
 int randomNumberInRange(int x, int y) {
     return rand() % (y - x + 1) + x;
+}
+
+// Generate a random string of a given length
+
+
+int main() {
+    // Random number between two values
+    printf("%d", randomNumberInRange(2, 5));
+    return 0;
 }
