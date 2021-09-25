f76876042c6588ae3a67d2b28332d0de3236f56c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-24 20:06:55 -0400

    Update array.c

--- C/array.c
+++ C/array.c
@@ -10,9 +10,21 @@ int main() {
     // Get a random value from the array
     int randomItem = randomElement(randomArray, 5);
     printf("%d\n", randomItem);
+    // Get the first element of the array
+    int firstValue = firstElement(randomArray, 5);
+    printf("%d\n", firstValue);
+    // Get the last element of the array
+    int lastValue = lastElement(randomArray, 5);
+    printf("%d\n", lastValue);
     // Remove all the values from the array
     clearArray(randomArray, 5);
     return 0;
+}
+
+// Get the length of the array
+int getArrayLength(int array[]) {
+    int arrSize = sizeof(array) / sizeof(array[0]);
+    return arrSize;
 }
 
 // Take in a input as an array and return a random element from the array
@@ -21,10 +33,14 @@ int randomElement(int array[], int size) {
     return array[randomIndex];
 }
 
-// Get the length of the array
-int getArrayLength(int array[]) {
-    int arrSize = sizeof(array) / sizeof(array[0]);
-    return arrSize;
+// Get the first element of the array
+int firstElement(int array[], int size) {
+    return array[0];
+}
+
+// Get the last element of the array
+int lastElement(int array[], int size) {
+    return array[size - 1];
 }
 
 // Remove all elements from the array
