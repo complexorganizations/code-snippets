66f9d691a3bdfbc1db87a810cd49aa2b87138b37
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-09-26 11:16:48 -0400

    update

--- Swift/function.swift
+++ Swift/function.swift
@@ -1,5 +1,39 @@
+// Simple function no arguments and no returns
+func noArgumentsNoReturns() {
+    print("No arguments no returns")
+}
+
+// Simple function with arguments and no returns
+func oneArgumentsNoReturns(content: String) {
+    print("One argument no returns: \(content)")
+}
+
+// Simple function with arguments and returns
+func oneArgumentsReturns(content: String) -> String {
+    return "One argument returns: \(content)"
+}
+
+// Simple function with two arguments and no returns
+func twoArgumentsNoReturns(content1: String, content2: String) {
+    print("Two arguments no returns: \(content1), \(content2)")
+}
+
+// Simple function with two arguments and returns
+func twoArgumentsReturns(content1: String, content2: String) -> (String, String) {
+    return ((content1), (content2))
+}
+
 func main() {
-    print("Hello, World!")
+    // No arguments no returns
+    noArgumentsNoReturns()
+    // One argument no returns
+    oneArgumentsNoReturns(content: "Hello")
+    // One argument returns
+    print(oneArgumentsReturns(content: "Hello"))
+    // Two arguments no returns
+    twoArgumentsNoReturns(content1: "Hello", content2: "World")
+    // Two arguments returns
+    print(twoArgumentsReturns(content1: "Hello", content2: "World"))
 }
 
 main()
--- Swift/variables.swift
+++ Swift/variables.swift
@@ -1,12 +1,55 @@
-// Bool
-var someBool = true
-print(someBool)
-// Int
-var someInt = 42
-print(someInt)
-// Double
-var someDouble = 3.14159
-print(someDouble)
-// String
-var someString = "Hello, world!"
-print(someString)
+// String variables
+func stringExamples() {
+    var firstName: String = "John"
+    var lastName: String = "Doe"
+    print(firstName + " " + lastName)
+}
+
+// Boolean variables
+func booleanExamples() {
+    var isMale: Bool = true
+    var isFemale: Bool = false
+    print(isMale, isFemale)
+}
+
+// Int variables
+func intExamples() {
+    var age: Int = 25
+    var weight: Int = 100
+    print(age, weight)
+}
+
+// Double variables
+func doubleExamples() {
+    var height: Double = 75.5
+    var hairSize: Double = 4.3
+    print(height, hairSize)
+}
+
+// Constants
+func constantExamples() {
+    let fullName: String = "John, Doe"
+    print(fullName)
+    let dobYear: Int = 1940
+    print(dobYear)
+    let isMarried: Bool = true
+    print(isMarried)
+    let heightInInches: Double = 75.5
+    print(heightInInches)
+}
+
+
+func main() {
+    // Examples of strings
+    stringExamples()
+    // Examples of booleans
+    booleanExamples()
+    // Examples of ints
+    intExamples()
+    // Examples of doubles
+    doubleExamples()
+    // Examples of constants
+    constantExamples()
+}
+
+main()
