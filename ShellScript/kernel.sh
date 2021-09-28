b9691fa5231e38306bb520fd72570df6cba39d9f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-28 19:12:51 -0400

    Update kernel.sh

--- ShellScript/kernel.sh
+++ ShellScript/kernel.sh
@@ -1,11 +1,5 @@
 # Grab the major release of the kernel.
-$()$(
-    awk </proc/version '{print $3}' | awk -F '-' '{print $1}' | awk -F\. '{print $1}'
-)$()
+< /proc/version awk '{print $3}' | awk -F '-' '{print $1}' | awk -F\. '{print $1}'
 # Grab the major release of the kernel with the minor version.
-$()$(
-    awk </proc/version '{print $3}' | cut -d'.' -f1-2
-)$()
-$()$(
-    uname -r | cut -d'.' -f1-2
-)$()
+< /proc/version awk '{print $3}' | cut -d'.' -f1-2
+uname -r | cut -d'.' -f1-2
