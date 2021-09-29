33551ffd2b906914a3ee760d513465ea4271c3c6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-29 11:02:01 -0400

    Update array.rs

--- Rust/array.rs
+++ Rust/array.rs
@@ -1,27 +1,10 @@
 fn main() {
-    let array_value = ["one", "two", "three"];
-    println!("{}", array_value[0]);
-    println!("{}", array_value[1]);
-    println!("{}", array_value[2]);
+    let random_list_of_elements = ["one", "two", "three"];
     // Get the length of the array
-    println!("{}", get_langth(&array_value));
-    // Get the first element of the array
-    println!("{}", get_first_element(&array_value));
-    // Get the last element of the array
-    println!("{}", get_last_element(&array_value));
+    println!("{}", length_of_array(&random_list_of_elements));
 }
 
 // Get the length of the array
-fn get_langth(some_array: &[&str]) -> usize {
-    some_array.len()
-}
-
-// Get the first element of the array
-fn get_first_element(some_array: &[&str]) -> &str {
-    some_array[0]
-}
-
-// Get the last element of the array
-fn get_last_element(some_array: &[&str]) -> &str {
-    some_array[some_array.len() - 1]
+fn length_of_array(provided_array: &[&str]) -> usize {
+    provided_array.len()
 }
