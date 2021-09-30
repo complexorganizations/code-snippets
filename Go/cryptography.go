1d35541ac9f397ec4f909f553eae34c2324a6cfd
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-30 19:36:17 -0400

    Update cryptography.go

--- Go/cryptography.go
+++ Go/cryptography.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 )
 
 func main() {
-	sum := sha256.Sum256([]byte("Hello, World!"))
-	fmt.Printf("%x", sum)
+	// Get the sha256 of a string
+	fmt.Println(getSHA256("Hello World"))
+	// Get the sha512 of a string
+	fmt.Println(getSHA512("Hello World"))
+}
+
+
+// Get the sha256 of a string
+func getSHA256(content string) string {
+	contentSHA256 := sha256.Sum256([]byte(content))
+	return fmt.Sprintf("%x", contentSHA256)
+}
+
+// Get the sha512 of a string
+func getSHA512(content string) string {
+	contentSHA512 := sha512.Sum512([]byte(content))
+	return fmt.Sprintf("%x", contentSHA512)
 }
