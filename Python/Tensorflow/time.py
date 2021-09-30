46f15e924fe4e81e0f9571486250c00e3471523e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-30 19:12:27 -0400

    Update time.py

--- Python/Tensorflow/time.py
+++ Python/Tensorflow/time.py
@@ -1,8 +1,14 @@
 import tensorflow
 
 
+# Get the current timestamp using tensorflow
+def get_current_timestamp():
+    return tensorflow.timestamp()
+
+
 def main():
-    print(tensorflow.timestamp())
+    # Get the current time
+    print(get_current_timestamp())
 
 
 main()
