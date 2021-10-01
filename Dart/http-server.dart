4091371993c6acc1c4e25f45d806f45a1fd54e8c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-01 12:19:35 -0400

    Update http-server.dart

--- Dart/http-server.dart
+++ Dart/http-server.dart
@@ -2,10 +2,9 @@ import "dart:io";
 
 main() async {
   var server = await HttpServer.bind("127.0.0.1", 8080);
+  exit(1);
   await for (var request in server) {
     request.response.write("Hello, World!");
     request.response.close();
-    break;
   }
-  exit(1);
 }
