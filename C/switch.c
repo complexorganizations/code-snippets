5e00c37c29aaebfeed34f8e40d8565a4621596bf
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-01 18:20:45 -0400

    Update switch.c

--- C/switch.c
+++ C/switch.c
@@ -6,16 +6,16 @@ void integerSwitch() {
     int age = 25;
     switch (age) {
         case 25:
-            printf("25");
+            printf("25\n");
             break;
         case 26:
-            printf("26");
+            printf("26\n");
             break;
         case 27:
-            printf("27");
+            printf("27\n");
             break;
         default:
-            printf("default");
+            printf("default\n");
     }
 }
 
@@ -24,16 +24,16 @@ void stringSwitch() {
     char name = 'John';
     switch (name) {
         case 'John':
-            printf("John");
+            printf("John\n");
             break;
         case 'Jane':
-            printf("Jane");
+            printf("Jane\n");
             break;
         case 'Joe':
-            printf("Joe");
+            printf("Joe\n");
             break;
         default:
-            printf("default");
+            printf("default\n");
     }
 }
 
