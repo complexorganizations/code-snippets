58d64272e966ffbd885aaed5fe9239ee048be4e3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-30 22:45:35 -0400

    Update http-server.js

--- JavaScript/http-server.js
+++ JavaScript/http-server.js
@@ -1,15 +1,1 @@
-const http = require("http")
-const { exit } = require("process")
-const host = "localhost"
-const port = 8000
 
-const requestListener = function (req, res) {
-    res.writeHead(200)
-    res.end("My first server!")
-}
-
-const server = http.createServer(requestListener)
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)
-    exit(1)
-})
