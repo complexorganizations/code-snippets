ee36c83c1008be1786fc1ab50782b8151e7beff3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-30 22:42:33 -0400

    Update http-client.js

--- JavaScript/http-client.js
+++ JavaScript/http-client.js
@@ -1,14 +1,1 @@
-const fetch = require("node-fetch")
 
-var raw = ""
-
-var requestOptions = {
-    method: "GET",
-    body: raw,
-    redirect: "follow"
-}
-
-fetch("https://api.ipengine.dev", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log("error", error))
