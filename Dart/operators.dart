fd1de371df10bab98e8fb9cb3af8538d1eec0c4a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-01 12:42:51 -0400

    Update operators.dart

--- Dart/operators.dart
+++ Dart/operators.dart
@@ -1,22 +1,54 @@
 void main() {
-  var fistName = "John";
-  var lastName = "Doe";
   // && operator is used to check if multiple value is true or false.
-  if (fistName == "John" && lastName == "Doe") {
-    print("Hello ${fistName} ${lastName}");
-  }
-  // || operator is used to check if one of the value is true or false.
-  if (fistName == "Jane" || lastName == "Doe") {
-    print("Hello ${fistName} ${lastName}");
-  }
+  bothStatementSame();
+  // || operator is used to check if any value is true or false.
+  bothStatementNotSame();
   // ! operator is used to reverse the value.
-  if (fistName != "John") {
-    print("${fistName}");
+  notStatement();
+  //
+  notStatementNotSame();
+  //
+  notStatementSame();
+}
+
+// && operator is used to check if multiple value is true or false.
+void bothStatementSame() {
+  var firstName = "John";
+  var lastName = "Doe";
+  if (firstName == "John" && lastName == "Doe") {
+    print("Hello ${firstName} ${lastName}");
   }
-  if (!(fistName == "John" && lastName == "Doe")) {
-    print("Hello ${fistName} ${lastName}");
+}
+
+// || operator is used to check if one of the value is true or false.
+void bothStatementNotSame() {
+  var firstName = "John";
+  var lastName = "Doe";
+  if (firstName != "John" && lastName != "Doe") {
+    print("Hello ${firstName} ${lastName}");
   }
-  if (fistName == "John" && lastName != "Doe") {
-    print("Hello ${fistName} ${lastName}");
+}
+
+// ! operator is used to reverse the value.
+void notStatement() {
+  var firstName = "John";
+  if (firstName != "John") {
+    print("Hello ${firstName}");
+  }
+}
+
+void notStatementNotSame() {
+  var firstName = "John";
+  var lastName = "Doe";
+  if (firstName == "John" && lastName != "Doe") {
+    print("Hello ${firstName} ${lastName}");
+  }
+}
+
+void notStatementSame() {
+  var firstName = "John";
+  var lastName = "Doe";
+  if (!(firstName == "John" && lastName == "Doe")) {
+    print("Hello ${firstName} ${lastName}");
   }
 }
