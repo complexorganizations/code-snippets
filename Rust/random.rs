94efd8980d54c314ad4598ad9457d955a1cd8ac0
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-01 20:06:53 -0400

    Update random.rs

--- Rust/random.rs
+++ Rust/random.rs
@@ -3,6 +3,8 @@ use rand::Rng;
 fn main() {
     // Generate a random boolean
     println!("{}", random_boolean());
+    // Random u8
+    println!("{}", random_eight_bit_integer());
 }
 
 // Generate a random boolean
@@ -10,4 +12,7 @@ fn random_boolean() -> bool {
     rand::thread_rng().gen::<bool>()
 }
 
-// Generate a random int
+// Generate a random 8 bit integer
+fn random_eight_bit_integer() -> u8 {
+    rand::thread_rng().gen::<u8>()
+}
