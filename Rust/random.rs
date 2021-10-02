bca73800c2b58d5f8a26cbbef20edf0bc9889a75
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-01 20:12:55 -0400

    Update random.rs

--- Rust/random.rs
+++ Rust/random.rs
@@ -5,6 +5,11 @@ fn main() {
     println!("{}", random_boolean());
     // Random u8
     println!("{}", random_eight_bit_integer());
+    // Random u16
+    println!("{}", random_sixteen_bit_integer());
+    // Random u32
+    println!("{}", random_thirtytwo_bit_integer());
+
 }
 
 // Generate a random boolean
@@ -16,3 +21,17 @@ fn random_boolean() -> bool {
 fn random_eight_bit_integer() -> u8 {
     rand::thread_rng().gen::<u8>()
 }
+
+// Generate a random 16 bit integer
+fn random_sixteen_bit_integer() -> u16 {
+    rand::thread_rng().gen::<u16>()
+}
+
+// Generate a random 32 bit integer
+fn random_thirtytwo_bit_integer() -> u32 {
+    rand::thread_rng().gen::<u32>()
+}
+
+// Generate a random 32 bit integer
+// println!("Random i32: {}", rng.gen::<i32>());
+// println!("Random float: {}", rng.gen::<f64>());
