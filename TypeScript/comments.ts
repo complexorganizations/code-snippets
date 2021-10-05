7d20ce2f7425b19b5cfef490b665d88a0e4c71fa
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-05 09:34:28 -0400

    Create comments.ts

---
+++ TypeScript/comments.ts
@@ -0,0 +1,16 @@
+// This is an in-line comment.
+
+function main() {
+    console.log("Hello, World")
+}
+
+main()
+
+// This
+// is
+// a
+// multi-line
+// comment.
+
+/* This is a
+multi-line comment */
