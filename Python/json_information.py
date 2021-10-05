ff73cfd7fb0238ae5d0432c1c4961617ef3efa1c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-05 09:08:05 -0400

    Update json_information.py

--- Python/json_information.py
+++ Python/json_information.py
@@ -1,10 +1,6 @@
 import json
 
 
-# Parse JSON and return a Python object:
-def json_decode(data):
-    return json.loads(data)
-
 
 # Check if the json is valid and return a boolean
 def is_json_valid(myjson):
@@ -15,16 +11,21 @@ def is_json_valid(myjson):
         return False
 
 
+# Parse JSON and return a Python object:
+def json_decode(data):
+    return json.loads(data)
+
+
 def main():
     # Some random json example
     validJson = '{"name":"John", "age":30, "city":"New York"}'
+    # Check if the json is valid
+    print(is_json_valid(validJson))
     # Parse the json
     jsonContent = json_decode(validJson)
     # Get the value by the key
     print(jsonContent["name"])
     print(jsonContent["age"])
-    # Check if the json is valid
-    print(is_json_valid(validJson))
 
 
 main()
