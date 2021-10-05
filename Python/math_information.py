6c2e243458d17a6763dc17140f253769a38e5047
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-04 22:43:34 -0400

    Update math_information.py

--- Python/math_information.py
+++ Python/math_information.py
@@ -37,73 +37,57 @@ def square_root(a):
 
 
 # Calculate the factorial of a number and return the result
-def factorial(a):
-    if a == 0:
+def factorial(num):
+    if num == 0:
         return 1
     else:
-        return a * factorial(a - 1)
-
+        return num * factorial(num - 1)
 
 # Calculate the average of a list of numbers and return the result
 def average(a):
-    if len(a) == 0:
-        return 0
-    else:
-        return sum(a) / len(a)
+    return sum(a) / len(a)
 
 
 # Calculate the median of a list of numbers and return the result
 def median(a):
-    if len(a) == 0:
-        return 0
+    sorted_a = sorted(a)
+    if len(sorted_a) % 2 == 0:
+        return (sorted_a[int(len(sorted_a) / 2)] + sorted_a[int(len(sorted_a) / 2) - 1]) / 2
     else:
-        sorted_a = sorted(a)
-        if len(sorted_a) % 2 == 0:
-            return (sorted_a[int(len(sorted_a) / 2)] + sorted_a[int(len(sorted_a) / 2) - 1]) / 2
-        else:
-            return sorted_a[int(len(sorted_a) / 2)]
+        return sorted_a[int(len(sorted_a) / 2)]
 
 
 # Calculate the mode of a list of numbers and return the result
 def mode(a):
-    if len(a) == 0:
-        return 0
-    else:
-        mode_dict = {}
-        for i in a:
-            if i in mode_dict:
-                mode_dict[i] += 1
-            else:
-                mode_dict[i] = 1
-        mode_list = []
-        for i in mode_dict:
-            if mode_dict[i] == max(mode_dict.values()):
-                mode_list.append(i)
-        return mode_list
+    mode_dict = {}
+    for i in a:
+        if i in mode_dict:
+            mode_dict[i] += 1
+        else:
+            mode_dict[i] = 1
+    mode_list = []
+    for i in mode_dict:
+        if mode_dict[i] == max(mode_dict.values()):
+            mode_list.append(i)
+    return mode_list
 
 
 # Calculate the standard deviation of a list of numbers and return the result
 def standard_deviation(a):
-    if len(a) == 0:
-        return 0
-    else:
-        mean = average(a)
-        variance = 0
-        for i in a:
-            variance += (i - mean) ** 2
-        return variance ** 0.5
+    mean = average(a)
+    variance = 0
+    for i in a:
+        variance += (i - mean) ** 2
+    return variance ** 0.5
 
 
 # Calculate the variance of a list of numbers and return the result
 def variance(a):
-    if len(a) == 0:
-        return 0
-    else:
-        mean = average(a)
-        variance = 0
-        for i in a:
-            variance += (i - mean) ** 2
-        return variance
+    mean = average(a)
+    variance = 0
+    for i in a:
+        variance += (i - mean) ** 2
+    return variance
 
 
 # Round the number to the nearest integer
