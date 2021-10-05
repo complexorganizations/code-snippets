99e4beb4bbb608ebdbe86654d8f01b8dadc91883
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-05 09:48:01 -0400

    Create terminal.cpp

---
+++ C++/terminal.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Clear the terminal screen
+void clearTerminal() {
+    printf("\033c");
+}
+
+// Take input from the user
+int getUserInput() {
+    int userInput;
+    printf("Enter an integer:");
+    scanf("%d", &userInput);
+    return userInput;
+}
+
+int main() {
+    // Clear the terminal
+    clearTerminal();
+    // Take input from the user
+    //int userInput = getUserInput();
+    //printf(userInput);
+    return 0;
+}
