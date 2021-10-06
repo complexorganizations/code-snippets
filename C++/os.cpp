21aee061a929299e0ec57419a97a3b47f2933136
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-06 07:28:07 -0400

    Update os.cpp

--- C++/os.cpp
+++ C++/os.cpp
@@ -1,17 +1,6 @@
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-// Get the current system hostname.
-char *get_hostname() {
-    char *hostname = malloc(HOST_NAME_MAX);
-    gethostname(hostname, HOST_NAME_MAX);
-    return hostname;
-}
+#include <iostream>
 
 int main() {
-    // Get the current system hostname.
-    printf("Hostname: %s\n", get_hostname());
+    printf("Hello, World!\n");
     return 0;
 }
