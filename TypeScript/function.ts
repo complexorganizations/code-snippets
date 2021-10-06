7d456640a797ae5df9c0c11f5828894e990dd7e5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-06 11:35:50 -0400

    Update function.ts

--- TypeScript/function.ts
+++ TypeScript/function.ts
@@ -1,35 +1,25 @@
-// Create a function main that takes no arguments and prints "Hello World".
 function main() {
-    console.log("Hello World")
     // Call the function functionImput with the argument "Hello World"
     functionImput("Hello, World!")
     // Call the function functionReturn with the argument "Hello World"
     console.log(functionReturn("Hello, World!"))
-    // Call the function addNumbers with the arguments 2 and 3
-    console.log(addNumbers(2, 3))
-    // Call the function subtractNumbers with the arguments 2 and 3
-    console.log(subtractNumbers(10, 2))
+    // Call the function that does some math and than returns the value.
+    console.log(doSomeMath(2, 3))
 }
 
-// Run the function main.
 main()
 
 // Create a function that takes an argument and prints it.
-function functionImput(content) {
+function functionImput(content: string) {
     console.log(content)
 }
 
 // Create a function that takes an argument and returns it.
-function functionReturn(content) {
+function functionReturn(content: string) {
     return content
 }
 
-// Create a function that takes two arguments and returns the sum of them.
-function addNumbers(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
-}
-
-// Create a function that takes two arguments and returns the difference of them.
-function subtractNumbers(firstNumber, secondNumber) {
-    return firstNumber - secondNumber
+// Create a function that takes two arguments and does some math and than returns 
+function doSomeMath(firstNumber: number, secondNumber: number) {
+    return firstNumber + secondNumber * secondNumber + firstNumber / secondNumber
 }
