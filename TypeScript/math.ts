400b81751c73fdc956c02756f498fe4503085d23
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-06 11:27:42 -0400

    Update math.ts

--- TypeScript/math.ts
+++ TypeScript/math.ts
@@ -1,11 +1,4 @@
 function main() {
-    // Do any math that you want
-    /*
-    console.log(basicMath(1, "+", 2))
-    console.log(basicMath(6, "*", 6))
-    console.log(basicMath(6, "/", 6))
-    console.log(basicMath(6, "-", 6))
-    */
     // Add two numbers and return the result
     console.log(add(1, 2))
     // Subtract two numbers and return the result
@@ -42,93 +35,86 @@ function main() {
 
 main()
 
-// Do any math that you want
-/*
-function basicMath(firstNumber,operation, secondNumber) {
-    return eval(firstNumber + operation + secondNumber)
-}
-*/
-
 // Add two numbers and return the result
-function add(firstNumber, secondNumber) {
+function add(firstNumber: number, secondNumber: number) {
     return firstNumber + secondNumber
 }
 
 // Subtract two numbers and return the result
-function subtract(firstNumber, secondNumber) {
+function subtract(firstNumber: number, secondNumber: number) {
     return firstNumber - secondNumber
 }
 
 // Multiply two numbers and return the result
-function multiply(firstNumber, secondNumber) {
+function multiply(firstNumber: number, secondNumber: number) {
     return firstNumber * secondNumber
 }
 
 // Divide two numbers and return the result
-function divide(firstNumber, secondNumber) {
+function divide(firstNumber: number, secondNumber: number) {
     return firstNumber / secondNumber
 }
 
 // Modulo two numbers and return the result
-function modulo(firstNumber, secondNumber) {
+function modulo(firstNumber: number, secondNumber: number) {
     return firstNumber % secondNumber
 }
 
 // Return the square root of a number
-function squareRoot(number) {
-    return Math.sqrt(number)
+function squareRoot(firstValue: number) {
+    return Math.sqrt(firstValue)
 }
 
 // Return the cube root of a number
-function cubeRoot(number) {
-    return Math.cbrt(number)
+function cubeRoot(firstValue: number) {
+    return Math.cbrt(firstValue)
 }
 
 // Return the nth root of a number
-function nthRoot(number, n) {
-    return Math.pow(number, 1 / n)
+function nthRoot(firstValue: number, n: number) {
+    return Math.pow(firstValue, 1 / n)
 }
 
 // Return the factorial of a number
-function factorial(number) {
+function factorial(firstValue: number) {
     let result = 1
-    for (let i = 1; i <= number; i++) {
-        result *= i
+    for (let i = 1; i <= firstValue; i++) {
+        result = result * i
     }
     return result
 }
 
 // Return the absolute value of a number
-function absoluteValue(number) {
-    return Math.abs(number)
+function absoluteValue(firstValue: number) {
+    return Math.abs(firstValue)
 }
 
 // Return the sine of an angle
-function sine(angle) {
+function sine(angle: number) {
     return Math.sin(angle)
 }
 
 // Return the cosine of an angle
-function cosine(angle) {
+function cosine(angle: number) {
     return Math.cos(angle)
 }
 
 // Return the tangent of an angle
-function tangent(angle) {
+function tangent(angle: number) {
     return Math.tan(angle)
 }
 
 // Return the arcsine of a number
-function arcsine(number) {
-    return Math.asin(number)
+function arcsine(firstValue: number) {
+    return Math.asin(firstValue)
 }
 
 // Return the arccosine of a number
-function arccosine(number) {
-    return Math.acos(number)
+function arccosine(firstValue: number) {
+    return Math.acos(firstValue)
 }
 
 // Return the arctangent of a number
-function arctangent(number) {
-    return Math.atan(number)
+function arctangent(firstValue: number) {
+    return Math.atan(firstValue)
 }
