0b96deeb1672f6ace1f5d680601f0057fcfe59ca
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-06 22:51:46 -0400

    Create comments.cs

---
+++ C#/comments.cs
@@ -0,0 +1,16 @@
+// This is an in-line comment.
+using System;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+/* This is a
+multi-line comment */
