043520b10bbdecc03b79ee8fde478555d352a8b7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-08 06:16:33 -0400

    Update switch.ts

--- TypeScript/switch.ts
+++ TypeScript/switch.ts
@@ -1,9 +1,11 @@
 function main() {
     switchStatement()
+    moreSwitchExample()
 }
 
 main()
 
+// A switch statement is a way to execute different code based on different conditions.
 function switchStatement() {
     var date = new Date()
     var day = date.getDay()
@@ -31,5 +33,23 @@ function switchStatement() {
             break
         default:
             console.log("Invalid day")
+    }
+}
+
+// More examples of switch statements
+function moreSwitchExample() {
+    var name: string = "John"
+    switch (name) {
+        case "John":
+            console.log("Hello John")
+            break
+        case "Mary":
+            console.log("Hello Mary")
+            break
+        case "Tom":
+            console.log("Hello Tom")
+            break
+        default:
+            console.log("Hello Stranger")
     }
 }
