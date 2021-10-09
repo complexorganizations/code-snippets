6f5a3688f13f2c1c43b2e12a8d8e1fa3859edbfe
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-09 19:30:23 -0400

    Update operators.ts

--- TypeScript/operators.ts
+++ TypeScript/operators.ts
@@ -5,6 +5,12 @@ function main(): void {
     console.log(check_two_strings_are_equal(first_name, last_name))
     // Check if the strings are not the same
     console.log(check_two_strings_are_not_equal(first_name, last_name))
+    // Check if the first and second value are true
+    console.log(basic_and_operator(false, true))
+    // Check if the first or second value is true
+    console.log(basic_or_operator(false, true))
+    // A simple example of a ! operator
+    console.log(basic_not_operator(true))
 }
 
 main()
@@ -17,4 +23,19 @@ function check_two_strings_are_equal(first_string: string, second_string: string
 // Check if two strings are not the same
 function check_two_strings_are_not_equal(first_string: string, second_string: string): boolean {
     return first_string != second_string
+}
+
+// A simple example of a && operator
+function basic_and_operator(first_value: boolean, second_value: boolean): boolean {
+    return first_value && second_value
+}
+
+// A simple example of a || operator
+function basic_or_operator(first_value: boolean, second_value: boolean): boolean {
+    return first_value || second_value
+}
+
+// A simple example of a ! operator
+function basic_not_operator(value: boolean): boolean {
+    return !value
 }
