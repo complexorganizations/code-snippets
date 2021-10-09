79bbab2138f9a7b1b6f10cd09b242c06675c804f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-09 18:00:57 -0400

    Update regex.ts

--- TypeScript/regex.ts
+++ TypeScript/regex.ts
@@ -1,9 +1,13 @@
 function main(): void {
-    // Regex for finding the first word in a string
-    var firstWord: RegExp = /\w+/
-    // The first word in the string
-    var first: RegExpExecArray | null = firstWord.exec("Hello World")
-    console.log(first)
+    // Match a string against a regular expression
+    const match: RegExpExecArray | null = regexMatch("Hello World", /\w+/)
+    console.log(match)
 }
 
 main()
+
+
+// Match a string against a regular expression
+function regexMatch(str: string, regex: RegExp): RegExpExecArray | null {
+    return regex.exec(str)
+}
