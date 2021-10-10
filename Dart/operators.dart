c81a15b84d604ea783ccfb0b90e066d6af60bd2c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-09 20:47:26 -0400

    Update operators.dart

--- Dart/operators.dart
+++ Dart/operators.dart
@@ -1,54 +1,16 @@
 void main() {
-  // && operator is used to check if multiple value is true or false.
-  bothStatementSame();
-  // || operator is used to check if any value is true or false.
-  bothStatementNotSame();
-  // ! operator is used to reverse the value.
-  notStatement();
-  //
-  notStatementNotSame();
-  //
-  notStatementSame();
+  // Check two statements are equal.
+  print(checkTwoStatementEqual("Apple", "Apple"));
+  // Check two statements are not equal.
+  print(checkTwoStatementNotEqual("Apple", "Banana"));
 }
 
-// && operator is used to check if multiple value is true or false.
-void bothStatementSame() {
-  var firstName = "John";
-  var lastName = "Doe";
-  if (firstName == "John" && lastName == "Doe") {
-    print("Hello ${firstName} ${lastName}");
-  }
+// Check if two statements are true.
+bool checkTwoStatementEqual(String first_statement, String second_statement) {
+  return first_statement == second_statement;
 }
 
-// || operator is used to check if one of the value is true or false.
-void bothStatementNotSame() {
-  var firstName = "John";
-  var lastName = "Doe";
-  if (firstName != "John" && lastName != "Doe") {
-    print("Hello ${firstName} ${lastName}");
-  }
-}
-
-// ! operator is used to reverse the value.
-void notStatement() {
-  var firstName = "John";
-  if (firstName != "John") {
-    print("Hello ${firstName}");
-  }
-}
-
-void notStatementNotSame() {
-  var firstName = "John";
-  var lastName = "Doe";
-  if (firstName == "John" && lastName != "Doe") {
-    print("Hello ${firstName} ${lastName}");
-  }
-}
-
-void notStatementSame() {
-  var firstName = "John";
-  var lastName = "Doe";
-  if (!(firstName == "John" && lastName == "Doe")) {
-    print("Hello ${firstName} ${lastName}");
-  }
+// Check if two statements are not equal
+bool checkTwoStatementNotEqual(String first_statement, String second_statement) {
+  return first_statement != second_statement;
 }
