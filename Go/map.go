d5fc5106bb9fb71287dac31cd5dea4084502e469
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-11 19:49:08 -0400

    Update map.go

--- Go/map.go
+++ Go/map.go
@@ -13,6 +13,10 @@ func main() {
 	fmt.Println(getFromMap(userMap, "One"))
 	// Get the key for a value in the map.
 	fmt.Println(getKeyFromMap(userMap, 1))
+	// Check if a key exists in the map.
+	fmt.Println(keyExistsInMap(userMap, "One"))
+	// Check if a value exists in the map.
+	fmt.Println(valueExistsInMap(userMap, 1))
 	// Removing a key/value pair from the map.
 	removeFromMap(userMap, "One")
 }
@@ -20,11 +24,6 @@ func main() {
 // Add a key/value pair to the map.
 func addToMap(userMap map[string]int, key string, value int) {
 	userMap[key] = value
-}
-
-// Remove a key/value pair from the map.
-func removeFromMap(userMap map[string]int, key string) {
-	delete(userMap, key)
 }
 
 // Get the value for a key in the map.
@@ -40,4 +39,25 @@ func getKeyFromMap(userMap map[string]int, value int) string {
 		}
 	}
 	return ""
+}
+
+// Check if a key exists in the map.
+func keyExistsInMap(userMap map[string]int, key string) bool {
+	_, keyExists := userMap[key]
+	return keyExists
+}
+
+// Check if a value exists in the map.
+func valueExistsInMap(userMap map[string]int, value int) bool {
+	for _, valueInMap := range userMap {
+		if valueInMap == value {
+			return true
+		}
+	}
+	return false
+}
+
+// Remove a key/value pair from the map.
+func removeFromMap(userMap map[string]int, key string) {
+	delete(userMap, key)
 }
