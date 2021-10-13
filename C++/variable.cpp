5f1ca1e6eee1a7f9f8d5fe5c079ddfc0efeeb57e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-12 22:18:10 -0400

    Update variable.cpp

--- C++/variable.cpp
+++ C++/variable.cpp
@@ -76,3 +76,18 @@ int main() {
     boolean_variable();
     return 0;
 }
+
+/*
+Escape Sequences Characters
+\b Backspace
+\f Form feed
+\n Newline
+\r Return
+\t Horizontal tab
+\v Vertical tab
+\\ Backslash
+\' Single quotation mark
+\" Double quotation mark
+\? Question mark
+\0 Null Character
+*/
