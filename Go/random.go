9b75b1da90b14c1cd4b3fff71e95052f11ab742b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-13 10:26:15 -0400

    Update random.go

--- Go/random.go
+++ Go/random.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	secureRandom "crypto/rand"
+	"crypto/rand"
 	"fmt"
 	"log"
 	"math/big"
-	"math/rand"
-	"time"
 )
 
 func main() {
@@ -17,57 +15,46 @@ func main() {
 	// Get a random string from array
 	arrayList := []string{"a", "b", "c", "d", "e"}
 	fmt.Println(getRandomStringFromArray(arrayList))
-	// Get the random integer between 0 and 10
-	fmt.Println(randomInt(0, 10))
 	// Generate a random bool
 	fmt.Println(randomBool())
 	// Generate a random byte array of given length
 	fmt.Println(randomByteArray(10))
 	// Generate a secure random int between 0 and 100
 	fmt.Println(secureRandomInt(100))
+	// Generate a random int between two numbers
+	fmt.Println(randomIntBetween(1, 10))
 }
 
 // Get a random string of length
 func randomString(bytesSize int) string {
-	rand.Seed(time.Now().UTC().UnixNano())
 	randomBytes := make([]byte, bytesSize/2)
 	rand.Read(randomBytes)
 	return fmt.Sprintf("%X", randomBytes)
 }
 
 // Get a random string with a specified length and characters.
-func randomStringSpecified(bytesSize int) string {
-	rand.Seed(time.Now().UTC().UnixNano())
+func randomStringSpecified(bytesSize int64) string {
 	// ABCDEFGHIJKLMNOPQRSTUVWXYZ, abcdefghijklmnopqrstuvwxyz, 0123456789 ~!@#$%^&*()-_+={}][|\`,./?;:'"<>
 	letters := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()-_+={}][|\\`,./?;:'\"<>")
-	randomString := make([]rune, bytesSize)
+	randomString := make([]rune, bytesSize/2)
 	for i := range randomString {
-		randomString[i] = letters[rand.Intn(len(letters))]
+		randomString[i] = letters[secureRandomInt(int64(len(letters)))]
 	}
 	return string(randomString)
 }
 
 // Get a random string from array
 func getRandomStringFromArray(arrayList []string) string {
-	rand.Seed(time.Now().UTC().UnixNano())
-	return arrayList[rand.Intn(len(arrayList))]
-}
-
-// generate a random integer
-func randomInt(min, max int) int {
-	rand.Seed(time.Now().UTC().UnixNano())
-	return rand.Intn(max-min) + min
+	return arrayList[secureRandomInt(int64(len(arrayList)))]
 }
 
 // Generate a random boolean value
 func randomBool() bool {
-	rand.Seed(time.Now().UTC().UnixNano())
-	return rand.Intn(2) == 1
+	return secureRandomInt(2) == 1
 }
 
 // Get a random byte array of given length
 func randomByteArray(length int) []byte {
-	rand.Seed(time.Now().UTC().UnixNano())
 	randomBytes := make([]byte, length)
 	rand.Read(randomBytes)
 	return randomBytes
@@ -75,9 +62,14 @@ func randomByteArray(length int) []byte {
 
 // Securely generate a random int
 func secureRandomInt(max int64) int64 {
-	randomValue, err := secureRandom.Int(secureRandom.Reader, big.NewInt(max))
+	someRandomNumber, err := rand.Int(rand.Reader, big.NewInt(max))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return randomValue.Int64()
+	return someRandomNumber.Int64()
+}
+
+// Generate a random int between two numbers
+func randomIntBetween(min int, max int) int64 {
+	return secureRandomInt(int64(max - min))
 }
