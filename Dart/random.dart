3b8d1dd3421a0875c22968c7b45f0a79e1ce30a6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-14 15:14:39 -0400

    Update random.dart

--- Dart/random.dart
+++ Dart/random.dart
@@ -3,10 +3,23 @@ import "dart:math";
 void main() {
   // Random number between two numbers
   print(getRandInt(1, 100));
+  // Generate a random string
+  print(getRandomString(10));
 }
 
 // Get a random number between a given range.
 int getRandInt(final int min, final int max) {
   final dynamic random = Random.secure();
   return random.nextInt(max - min);
+}
+
+// Generate a random string of a given length.
+String getRandomString(final int length) {
+  const String characterList =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  final StringBuffer buffer = StringBuffer();
+  for (int i = 0; i < length; i++) {
+    buffer.write(characterList[Random.secure().nextInt(characterList.length)]);
+  }
+  return buffer.toString();
 }
