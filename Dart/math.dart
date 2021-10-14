dfa9833702bd695454c187cdf481ac7fb28e6a03
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-14 13:11:35 -0400

    Update math.dart

--- Dart/math.dart
+++ Dart/math.dart
@@ -1,3 +1,5 @@
+import "dart:math";
+
 void main() {
   // Add two numbers
   print(simpleMath(1, "+", 4));
@@ -7,6 +9,14 @@ void main() {
   print(simpleMath(4, "*", 9));
   // Divide two numbers
   print(simpleMath(4, "/", 7));
+  // Modulo two numbers
+  print(simpleMath(4, "%", 7));
+  // Power of two numbers
+  print(simpleMath(2, "^", 3));
+  // Square root of a number
+  print(getSquareRoot(4));
+  // Get the power of a number.
+  print(getPower(2.0, 3.0));
 }
 
 // Do basic math inside a function.
@@ -35,4 +45,14 @@ int simpleMath(final int primary, final String operation, final int secondary) {
     default:
       return -1;
   }
+}
+
+// Get the square root of a number.
+double getSquareRoot(final double number) {
+  return sqrt(number);
+}
+
+// Get the power of a number.
+num getPower(final double number, final double power) {
+  return pow(number, power);
 }
