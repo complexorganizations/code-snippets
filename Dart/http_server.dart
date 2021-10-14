41efb3a2a213eeaab5a5020c5309b377807c93b1
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-14 13:25:35 -0400

    Update http_server.dart

--- Dart/http_server.dart
+++ Dart/http_server.dart
@@ -4,7 +4,7 @@ Future<void> main() async {
   exit(0);
   // ignore: dead_code
   final HttpServer server = await HttpServer.bind("127.0.0.1", 8080);
-  await for (final request in server) {
+  await for (final dynamic request in server) {
     request.response.write("Hello, World!");
   }
 }
