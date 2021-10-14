898a02c08bf702f15b49cec34852d032f08237e6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-14 15:04:47 -0400

    Update os.dart

--- Dart/os.dart
+++ Dart/os.dart
@@ -1,36 +1,35 @@
 import "dart:io";
 
 void main() {
-  // Check the platform version.
-  print(Platform.version);
-  // Check if the current operating system is Android.
-  print(Platform.isAndroid);
-  // Check if the current operating system is iOS.
-  print(Platform.isIOS);
-  // Check if the current operating system is macOS.
-  print(Platform.isMacOS);
-  // Check if the current operating system is Windows.
-  print(Platform.isLinux);
-  // Check if the current operating system is FreeBSD.
-  print(Platform.isWindows);
-  // Check if the current operating system is Fuchsia.
-  print(Platform.isFuchsia);
-  print(Platform.environment);
-  print(Platform.executable);
-  // Get the current operating system's name.
-  print(Platform.operatingSystem);
-  // Get the current operating system's version.
-  print(Platform.operatingSystemVersion);
-  // Get the current hostname
-  print(Platform.localHostname);
-  print(Platform.resolvedExecutable);
-  print(Platform.script);
-  print(Platform.script.authority);
-  print(Platform.script.path);
-  print(Platform.script.scheme);
-  // Check if the current application is running on a docker container.
+  // Check if the current app is running inside docker container.
   print(isRunningInDocker());
-  // Exit the program.
+  // Get the current operating system.
+  print(getOperatingSystemName());
+  // Get the current operating system version.
+  print(getOperatingSystemVersion());
+  // Get the current local hostname.
+  print(getLocalHostname());
+  // Check if the current os is android.
+  print(isAndroid());
+  // Check if the current os is ios.
+  print(isIOS());
+  // Check if the current os is windows.
+  print(isWindows());
+  // Check if the current os is macos.
+  print(isMacOS());
+  // Check if the current os is linux.
+  print(isLinux());
+  // Check if the current os is fushsia.
+  print(isFuchsia());
+  // Get the current platform environment.
+  print(getPlatformEnvironment());
+  // Get the current platform executable.
+  print(getPlatformExecutable());
+  // Get the current platform resolved executable.
+  print(getPlatformResolvedExecutable());
+  // Get the number of processors.
+  print(getNumberOfProcessors());
+  // Close the current app.
   closeApplication(0);
 }
 
@@ -44,11 +43,6 @@ bool isRunningInDocker() {
   }
 }
 
-// Simple exit function
-void closeApplication(final int code) {
-  exit(code);
-}
-
 // Current operating system
 String getOperatingSystemName() {
   return Platform.operatingSystem;
@@ -59,7 +53,62 @@ String getOperatingSystemVersion() {
   return Platform.operatingSystemVersion;
 }
 
-// Current hostname
-String getHostname() {
+// Current local hostname
+String getLocalHostname() {
   return Platform.localHostname;
+}
+
+// Check if the current os is andorid
+bool isAndroid() {
+  return Platform.isAndroid;
+}
+
+// Check if the current os is ios
+bool isIOS() {
+  return Platform.isIOS;
+}
+
+// Check if the current os is macos
+bool isMacOS() {
+  return Platform.isMacOS;
+}
+
+// Check if the current os is windows
+bool isWindows() {
+  return Platform.isWindows;
+}
+
+// Check if the current os is linux
+bool isLinux() {
+  return Platform.isLinux;
+}
+
+// Check if the current os is fuchsia
+bool isFuchsia() {
+  return Platform.isFuchsia;
+}
+
+// Get the current platform environment variable.
+Map<String, String> getPlatformEnvironment() {
+  return Platform.environment;
+}
+
+// Get the current platform executable.
+String getPlatformExecutable() {
+  return Platform.executable;
+}
+
+// Get the current platform resolvedExecutable.
+String getPlatformResolvedExecutable() {
+  return Platform.resolvedExecutable;
+}
+
+// Get the number of processors.
+int getNumberOfProcessors() {
+  return Platform.numberOfProcessors;
+}
+
+// Simple exit function
+void closeApplication(final int code) {
+  exit(code);
 }
