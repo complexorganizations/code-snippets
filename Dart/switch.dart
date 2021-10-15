fb0ff92ccd9a1520034b06b70aceb4443ee8835a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-14 21:28:02 -0400

    Update switch.dart

--- Dart/switch.dart
+++ Dart/switch.dart
@@ -2,6 +2,8 @@ void main() {
   simpleSwitch("+");
   // more example
   moreExample();
+  // return example
+  print(returnExample(1));
 }
 
 void simpleSwitch(final String operator) {
@@ -34,5 +36,17 @@ void moreExample() {
       break;
     default:
       print("other");
+  }
+}
+
+// Return inside a switch statement
+int returnExample(final int value) {
+  switch (value) {
+    case 1:
+      return 1;
+    case 2:
+      return 2;
+    default:
+      return -1;
   }
 }
