ea44c42c2e3c4d6d622b86fd616d4555d6bfc2ca
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-16 12:09:27 -0400

    Update array.rs

--- Rust/src/array.rs
+++ Rust/src/array.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::needless_return)]
+#![allow(clippy::needless_return)]
 fn main() {
     let random_list_of_elements = ["one", "two", "three", "four", "five"];
     // Get the length of the array
