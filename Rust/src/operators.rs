99b7a74faa09645ef80a9099841bca1a0e2efc82
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-16 15:56:49 -0400

    Update operators.rs

--- Rust/src/operators.rs
+++ Rust/src/operators.rs
@@ -7,6 +7,14 @@ fn main() {
     and_operator();
     // || Operator is a or operator, and checks if either condition is true.
     or_operator();
+    // < Operator checks if the first value is less than the second value.
+    less_than();
+    // > Operator checks if the first value is more than the second value.
+    more_than();
+    // =< Operator checks if the first value is less than or equal to second value.
+    less_than_or_equal_to();
+    // >= Operator checks if the first value is more than or equal to second value.
+    more_than_or_equal_to();
 }
 
 // == Operator checks if two things are the same.
@@ -42,5 +50,41 @@ fn or_operator() {
     let second_value: bool = false;
     if first_value || second_value {
         println!("One of the values is true.");
+    }
+}
+
+// < Operator checks if the first value is less than the second value.
+fn less_than() {
+    let first_value: i32 = 1;
+    let second_value: i32 = 2;
+    if first_value < second_value {
+        println!("The first value is less than the second value.");
+    }
+}
+
+// > Operator checks if the first value is more than the second value.
+fn more_than() {
+    let first_value: i32 = 1;
+    let second_value: i32 = 2;
+    if first_value > second_value {
+        println!("The first value is more than the second value.");
+    }
+}
+
+// =< Operator checks if the first value is less than or equal to second value.
+fn less_than_or_equal_to() {
+    let first_value: i32 = 1;
+    let second_value: i32 = 2;
+    if first_value <= second_value {
+        println!("The first value is less than or equal to the second value.");
+    }
+}
+
+// >= Operator checks if the first value is more than or equal to second value.
+fn more_than_or_equal_to() {
+    let first_value: i32 = 1;
+    let second_value: i32 = 2;
+    if first_value >= second_value {
+        println!("The first value is more than or equal to the second value.");
     }
 }
