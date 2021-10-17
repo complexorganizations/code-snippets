10b10e70bcf94155d63912baf971fcbfa98f14a4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-17 08:02:38 -0400

    Update functions.cpp

--- C++/functions.cpp
+++ C++/functions.cpp
@@ -6,31 +6,26 @@ void no_return_no_arguments() {
 }
 
 // Simple function that takes an integer argument and returns nothing
-void no_return_one_argument(int x) {
-    printf("%d", x);
+void no_return_one_argument(int first_value) {
+    printf("%d", first_value);
 }
 
 // Simple function that takes in two integers and returns nothing
-void no_return_two_arguments(int x, int y) {
-    printf("%d %d", x, y);
+void no_return_two_arguments(int first_value, int second_value) {
+    printf("%d %d", first_value, second_value);
 }
 
 // Simple function that takes in an integer and returns an integer
-int one_return_one_argument(int x) {
-    return x;
+int one_return_one_argument(int first_value) {
+    return first_value;
 }
 
 // Simple function that takes in two integers and returns an integer
-int one_return_two_arguments(int x, int y) {
-    return x + y;
+int one_return_two_arguments(int first_value, int second_value) {
+    return first_value + second_value;
 }
 
-// Simple function that takes in two integers and returns two integers
-int two_return_two_arguments(int x, int y) {
-    return x, y;
-}
-
-// A simple example of goto in C
+// A simple efirst_valueample of goto in C
 void simple_goto() {
     goto simple_block;
 
@@ -46,14 +41,9 @@ int main() {
     // Two arguments and no return value
     no_return_two_arguments(1, 2);
     // One argument and one return value
-    int the_return_value_of_one_argument = one_return_one_argument(1);
-    printf("%d\n", the_return_value_of_one_argument);
+    printf("%d\n", one_return_one_argument(1));
     // Two arguments and one return value
-    int the_return_value_of_two_arguments = one_return_two_arguments(1, 2);
-    printf("%d\n", the_return_value_of_two_arguments);
-    // Two arguments and two return values
-    int first_return_of_two_returns, second_return_of_two_returns = two_return_two_arguments(1, 2);
-    printf("%d %d\n", first_return_of_two_returns, second_return_of_two_returns);
+    printf("%d\n", one_return_two_arguments(1, 2));
     // Simple goto
     simple_goto();
     return 0;
