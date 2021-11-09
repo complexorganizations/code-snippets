deacec1e91f7f464800fd9e674e455570cb35689
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-08 20:41:18 -0500

    Create encode-json.go

---
+++ Go/JSON/encode-json.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Encode data from a map to JSON
+	type ColorGroup struct {
+		ID     int
+		Name   string
+		Colors []string
+	}
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	currentJson, err := json.Marshal(group)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(CurrentJson)
+}
