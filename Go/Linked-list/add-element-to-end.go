88855a506c4e88c256e5a24d7eeb9686c61d5314
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-11-10 01:06:45 -0500

    added linked list

---
+++ Go/Linked-list/add-element-to-end.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"container/list"
+)
+
+func main() {
+	// Create a new linked list.
+	linkedList := list.New()
+	// Add a new element to the end of the linked list.
+	element := addAtEndOfLinkedList(linkedList, 1)
+	// Print the linked list.
+	fmt.Printf("%v\n", element)
+}
+
+// Add a new element to the end of the linked list.
+func addAtEndOfLinkedList(linkedList *list.List, value int) *list.Element {
+	return linkedList.PushBack(value)
+}
