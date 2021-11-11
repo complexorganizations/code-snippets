5c44a427d2cab009c692414d5a2189d411cba38d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-11 11:36:38 -0500

    Update get-user-ip.go

--- Go/Network/get-user-ip.go
+++ Go/Network/get-user-ip.go
@@ -6,12 +6,12 @@ import (
 	"log"
 	"net"
 	"net/http"
-	//"os"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", helloHandler)
-	//os.Exit(0) // Remove this line; it's there to ensure that automated testing doesn't continue on indefinitely.
+	os.Exit(0) // Remove this line; it's there to ensure that automated testing doesn't continue on indefinitely.
 	// Listen and serve on port 8080.
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
