b8435c2cf52059471d5d51de767ebda6fc5755b5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-11 10:14:52 -0500

    Update write-to-file.go

--- Go/File/write-to-file.go
+++ Go/File/write-to-file.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	// Write to file without appending anything.
-	writeToFile("foo.txt", "Hello World")
+	writeToFile("bar.txt", "Hello World")
 }
 
 // Don't append and write to file
-func writeToFile(filepath string, content string) {
-	err := os.WriteFile(content, []byte(filepath), 0644)
+func writeToFile(path string, content string) {
+	err := os.WriteFile(path, []byte(content), 0644)
 	if err != nil {
 		log.Fatalln(err)
 	}
