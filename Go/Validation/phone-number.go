e279ad04806ab1a937039e606a627a1ffdc76cc0
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-15 01:26:08 -0500

    Create phone-number.go

---
+++ Go/Validation/phone-number.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nyaruka/phonenumbers"
+)
+
+func main() {
+	// Check if the phone number provided is valid
+	fmt.Println(validatePhoneNumber("+1-508–253–5000"))
+}
+
+// Check if the phone number provided is valid
+func validatePhoneNumber(phoneNumber string) bool {
+	number, err := phonenumbers.Parse(phoneNumber, "US")
+	if err != nil {
+		return false
+	}
+	return phonenumbers.IsValidNumber(number)
+}
