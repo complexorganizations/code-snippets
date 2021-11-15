86c8022b0a03c459672c1616bc5071ed0befb01e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-15 01:03:16 -0500

    Update append-write-string-to-file.go

--- Go/File/append-write-string-to-file.go
+++ Go/File/append-write-string-to-file.go
@@ -11,8 +11,8 @@ func main() {
 }
 
 // Append and write to file
-func appendAndWriteToFile(pathInSystem string, content string) {
-	filePath, err := os.OpenFile(pathInSystem, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func appendAndWriteToFile(path string, content string) {
+	filePath, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalln(err)
 	}
