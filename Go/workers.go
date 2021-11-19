8bb0a75a8c3d13bbbcd424bec000b4d358a4c5b6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-19 12:04:14 -0500

    Update workers.go

--- Go/workers.go
+++ Go/workers.go
@@ -10,6 +10,7 @@ var (
 )
 
 func main() {
+	// Create a certain numbers of go routine, with waitgroup
 	for loop := 0; loop <= 1000; loop++ {
 		waitGroup.Add(1)
 		go secondFunction()
@@ -17,6 +18,7 @@ func main() {
 	waitGroup.Wait()
 }
 
+// Complete a certain function and tell waitgroup it's completed.
 func secondFunction() {
 	fmt.Println("Done")
 	waitGroup.Done()
