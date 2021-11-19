36a38da34d2b15803c1971f45f95f341d5743911
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-19 13:22:37 -0500

    Update remove-directory.ts

--- TypeScript/Directory/remove-directory.ts
+++ TypeScript/Directory/remove-directory.ts
@@ -2,7 +2,7 @@ import * as fs from "fs"
 
 function main(): void {
     // Remove a directory.
-    removeDirectory("foo/")
+    removeDirectory("assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9/")
 }
 
 // Remove a directory.
