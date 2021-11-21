fc88d978aad8174c602a63a0b63c3d9058dcfa17
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-11-21 11:42:40 -0500

    update

---
+++ TypeScript/String/trim-a-string.ts
@@ -0,0 +1,11 @@
+function main(): void {
+    // Trim a string and return it
+    console.log(trimAString(" Hello World "))
+}
+
+main()
+
+// Trim a string and return it
+function trimAString(content: string): string {
+    return content.trim()
+}
