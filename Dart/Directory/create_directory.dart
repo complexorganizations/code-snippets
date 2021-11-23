23d96af3f89f7986c5bd0e046d074e6c0e53e649
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-22 22:24:03 -0500

    Create create_directory.dart

---
+++ Dart/Directory/create_directory.dart
@@ -0,0 +1,12 @@
+import "dart:core";
+import "dart:io";
+
+void main() {
+  // Create a directory
+  createDirectory("assets/remove/9WpmjE2856TAk3gNFKA5ypg5YL2b6D2r")
+}
+
+// Create a directory in a given path
+void createDirectory(final String path) {
+  Directory(path).createSync();
+}
