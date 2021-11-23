708b3080dd3fef9ba7db34dd39e05f2e5cb9e192
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-22 22:29:12 -0500

    Update remove_directory.dart

--- Dart/Directory/remove_directory.dart
+++ Dart/Directory/remove_directory.dart
@@ -3,7 +3,7 @@ import "dart:io";
 
 void main() {
   // Remove a directory
-  removeDirectory("assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9/")
+  removeDirectory("assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9");
 }
 
 // Remove a directory
