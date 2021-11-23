a2838d0a12a4ab43c7afd82ff16a58452912c2b2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-22 22:37:01 -0500

    Create if_else_statement.dart

---
+++ Dart/Flow-control/if_else_statement.dart
@@ -0,0 +1,11 @@
+import "dart:core";
+
+void main() {
+  final int primary = 10;
+  final int secondary = 20;
+  if (primary == secondary) {
+    print("They are both the same value.");
+  } else {
+    print("They are no the same.");
+  }
+}
