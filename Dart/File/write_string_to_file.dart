48277796408570caf14c03f1bb03c4d4845d2bba
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-26 11:02:33 -0500

    Update write_string_to_file.dart

--- Dart/File/write_string_to_file.dart
+++ Dart/File/write_string_to_file.dart
@@ -3,10 +3,14 @@ import "dart:io";
 
 void main() {
   // Write string to a file
-  writeStringToFile("assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9/4bksX68uXV7v3FK69bf99KhjFz5aCY3P", "7Za2hebkUn5FYaqEiznJ5R65dJ77mgiy92KYzrMnVjdkyPWMqAQj5TmQRN8zw5Z9Ke7r23UsFEGCfx3gFmLavCuQYx46A8EskqWK4FWe894SNo2arVvWBMU79GvKLPtC");
+  writeStringToFile("assets/remove/89mmtNQY7hM7389f", "7Za2hebkUn5FYaqEiznJ5R");
 }
 
 // Write string to a file
 void writeStringToFile(final String path, final String content) {
-  File(path).writeAsString(content);
+  try {
+    File(path).writeAsString(content);
+  } on Exception catch (e) {
+    print(e);
+  }
 }
