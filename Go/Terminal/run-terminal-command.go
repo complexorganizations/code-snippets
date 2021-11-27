f7c3a81cea1b329732f5720aaaab89bbf7eec415
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-27 08:41:36 -0500

    Create run-terminal-command.go

---
+++ Go/Terminal/run-terminal-command.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func main() {
+	// Run a command in the system terminal.
+	runSystemTerminalCommand("clear")
+}
+
+// Run a command in the system terminal.
+func runSystemTerminalCommand(content string) {
+	cmd := exec.Command(content)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
