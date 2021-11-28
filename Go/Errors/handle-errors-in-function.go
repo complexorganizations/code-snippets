9fad487c77e2ba142ffc35ead940ea3e75a4968e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-28 00:56:56 -0500

    Update handle-errors-in-function.go

--- Go/Errors/handle-errors-in-function.go
+++ Go/Errors/handle-errors-in-function.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// Handle all errors at the function level.
-	err := os.MkdirAll("assets/remove/6VoNjRbnPXfmLpW1WyWt")
+	err := os.MkdirAll("assets/remove/6VoNjRbnPXfmLpW1WyWt", 0755)
 	if err != nil {
 		log.Fatalln(err)
 	}
