f6954b017c9826f572fa2b869d24f210a8c29d11
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-28 12:13:45 -0500

    Update and rename read-a-file.go to read-a-file-as-string.go

--- Go/File/read-a-file-as-string.go
+++ Go/File/read-a-file-as-string.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	// Read a file and print the contents
-	fmt.Println(readAFile("assets/valid/valid-txt.txt"))
+	fmt.Println(readAFileAsString("assets/valid/valid-txt.txt"))
 }
 
 // Read a file and return the contents
-func readAFile(path string) string {
+func readAFileAsString(path string) string {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatalln(err)
