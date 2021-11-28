87fac03e0f9fa74f4b4f081df127d0fc208a232c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-28 08:53:39 -0500

    Create remove-exif-from-png.go

---
+++ Go/Images/remove-exif-from-png.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+func main() {
+	// Remove all the exif data from an png file.
+	removeEXIFDataFromPNGFromFile("assets/valid/valid-png.png")
+}
+
+// Remove all the EXIF data from an PNG file
+func removeEXIFDataFromPNGFromFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	outfile, err := os.Create(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	png.Encode(outfile, img)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
