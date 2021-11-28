a77b47a723b0d8ed0ade610fef0920aa0ccee011
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-11-28 08:45:36 -0500

    Create remove-exif-from-jpeg.go

---
+++ Go/Images/remove-exif-from-jpeg.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"image"
+	"image/jpeg"
+	"log"
+	"os"
+)
+
+func main() {
+	// Remove EXIF Data from JPEG file.
+	removeEXIFDataFromJPEGFile("assets/valid/valid-jpg.jpg")
+}
+
+// Remove EXIF Data from JPEG file.
+func removeEXIFDataFromJPEGFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	outfile, err := os.Create(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	jpeg.Encode(outfile, img, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
