65b35b06cafe280778e8945ac89b0a1c967a3482
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2021-11-30 11:35:12 -0500

    update

---
+++ Go/Math/get-mean-of-int-in-slice.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Create a slice of ints.
+	randomData := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//
+}
+
+// Get the mean of ints in a slice.
+func getMeanOfIntInSlice(slice []float64) float64 {
+	
+}
