a66a9119566eb1739cb3c1d1c54df4b25274de18
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-01 17:50:29 -0500

    Update element-counter-in-slice.go

--- Go/Slice/element-counter-in-slice.go
+++ Go/Slice/element-counter-in-slice.go
@@ -14,8 +14,8 @@ func main() {
 // Count how many times an element appears in a slice.
 func countElementInSlice(slice []string, element string) int {
 	var counter int
-	for _, element := range slice {
-		if element == element {
+	for _, value := range slice {
+		if value == element {
 			counter = counter + 1
 		}
 	}
