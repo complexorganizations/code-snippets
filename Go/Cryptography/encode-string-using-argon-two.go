21dc212cf88ba87192187c6289acbc459bccaa67
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-02 10:27:38 -0500

    Update encode-string-using-argon-two.go

--- Go/Cryptography/encode-string-using-argon-two.go
+++ Go/Cryptography/encode-string-using-argon-two.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	//"crypto/rand"
-	//"encoding/hex"
+	// "crypto/rand"
+	// "encoding/hex"
 	"fmt"
-	//"log"
-
-	//"golang.org/x/crypto/argon2"
+	// "log"
+	// "math/big"
+	// "golang.org/x/crypto/argon2"
 )
 
 /*
@@ -20,19 +20,21 @@ type argonConfig struct {
 }
 
 var argonConfiguration = &argonConfig{
-	keyLength: 512,
-	memory:    65536,
-	salt:      make([]byte, 512),
-	threads:   4,
-	time:      60,
+	keyLength: uint32(randomIntBetween(512, 1024)),
+	memory:    uint32(randomIntBetween(262144, 524288)),
+	salt:      make([]byte, randomIntBetween(512, 1024)),
+	threads:   uint8(randomIntBetween(16, 32)),
+	time:      uint32(randomIntBetween(150, 300)),
 }
+*/
 
 func main() {
+	fmt.Println("Hello, World!")
 	// Generate a hash of the given password
-	passwordHash := encodeStringUsingArgonTwo(argonConfiguration, "WHV02Qkhv7JhD8g4YFt8")
-	fmt.Println(passwordHash)
+	// fmt.Println(encodeStringUsingArgonTwo(argonConfiguration, "WHV02Qkhv7JhD8g4YFt8"))
 }
 
+/*
 func encodeStringUsingArgonTwo(argonSettings *argonConfig, password string) string {
 	// Generate a random salt
 	_, err := rand.Read(argonSettings.salt)
@@ -42,8 +44,18 @@ func encodeStringUsingArgonTwo(argonSettings *argonConfig, password string) stri
 	// Generate a hash
 	return fmt.Sprintf("%d,%d,%d,%s,%s", argonSettings.memory, argonSettings.time, argonSettings.threads, hex.EncodeToString(argonSettings.salt), hex.EncodeToString(argon2.IDKey([]byte(password), argonSettings.salt, argonSettings.time, argonSettings.memory, argonSettings.threads, argonSettings.keyLength)))
 }
-*/
 
-func main() {
-	fmt.Println("Hello, World!")
+// Generate a random int between two numbers
+func randomIntBetween(min int, max int) int {
+	return generateRandomInt(int64(max-min)) + min
 }
+
+// Generate a random int between 0 and a given max
+func generateRandomInt(max int64) int {
+	randomInt, err := rand.Int(rand.Reader, big.NewInt(max))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return int(randomInt.Int64())
+}
+*/
