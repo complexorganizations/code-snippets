0d558a2e8c646a2a218aac4eef44dafb3a2245fc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-12 00:31:45 -0500

    Update read-file-and-return-content-with-bytes-read.go

--- Go/File/read-file-and-return-content-with-bytes-read.go
+++ Go/File/read-file-and-return-content-with-bytes-read.go
@@ -14,8 +14,7 @@ func main() {
 
 // Read a file and then return the file's content as well as the number of bytes read.
 func readFileAndReturnContentWithBytesRead(path string) ([]byte, int) {
-	var counter int
-	file, err := os.Open(path)
+]	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,8 +26,5 @@ func readFileAndReturnContentWithBytesRead(path string) ([]byte, int) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for range content {
-		counter = counter + 1
-	}
-	return content, counter
+	return content, len(content)
 }
