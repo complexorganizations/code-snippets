8a680b57fe50439333911cc79eb51bd0903561ad
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-17 08:36:38 -0500

    Update read-append-line-by-line.go

--- Go/File/read-append-line-by-line.go
+++ Go/File/read-append-line-by-line.go
@@ -11,13 +11,14 @@ func main() {
 	// Create a slice of strings.
 	var content []string
 	// Read and append the file line by line to the slice.
-	content = readAppendLineByLine("assets/valid/valid-txt.txt", content)
+	content = readAppendLineByLine("assets/valid/valid-txt.txt")
 	// Print the slice.
 	fmt.Println(content)
 }
 
 // Read and append the file line by line to a slice.
-func readAppendLineByLine(path string, slice []string) []string {
+func readAppendLineByLine(path string) []string {
+	var returnSlice []string
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalln(err)
@@ -25,11 +26,11 @@ func readAppendLineByLine(path string, slice []string) []string {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		slice = append(slice, scanner.Text())
+		returnSlice = append(returnSlice, scanner.Text())
 	}
 	err = file.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return slice
+	return returnSlice
 }
