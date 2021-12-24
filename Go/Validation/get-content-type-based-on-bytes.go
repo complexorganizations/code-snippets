24426645b24080d73149302a9c08d2a7c36031f3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-24 00:13:53 -0500

    Update get-content-type-based-on-bytes.go

--- Go/Validation/get-content-type-based-on-bytes.go
+++ Go/Validation/get-content-type-based-on-bytes.go
@@ -15,7 +15,7 @@ func main() {
 
 // Get the content type of bytes
 func getContentTypeBasedOnBytes(content []byte) string {
-	buffer := make([]byte, 512)
+	buffer := make([]byte, len(content))
 	_, err := io.ReadFull(bytes.NewReader(content), buffer)
 	if err != nil {
 		log.Fatalln(err)
