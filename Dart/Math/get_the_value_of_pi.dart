dc8d781a499016f9fc851363fe4bdc75c6c6f801
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-31 04:19:19 -0500

    Create get_the_value_of_pi.dart

---
+++ Dart/Math/get_the_value_of_pi.dart
@@ -0,0 +1,12 @@
+import "dart:core";
+import "dart:math";
+
+void main() {
+    // Get the value of pi.
+    print(getValueOfPi());
+}
+
+// Get the value of pi
+double getValueOfPi() {
+  return pi;
+}
