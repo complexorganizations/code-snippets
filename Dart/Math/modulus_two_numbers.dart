54ee1e30be094ba9f092a526e6e518b6ed1482b8
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-31 04:12:56 -0500

    Create modulus_two_numbers.dart

---
+++ Dart/Math/modulus_two_numbers.dart
@@ -0,0 +1,11 @@
+import "dart:core";
+
+void main() {
+    // Find the modulus of two numbers
+    print(modulusOfTwoNumbers(10, 2));
+}
+
+// modulus two numbers and return the result
+int modulusOfTwoNumbers(final int primary, final int secondary) {
+    return primary % secondary;
+}
