9f9b4900d61acd8dcfb122e8050f0af4bf1c1748
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-03 13:59:26 -0500

    Update get-url-path.go

--- Go/Network/get-url-path.go
+++ Go/Network/get-url-path.go
@@ -24,9 +24,8 @@ func helloHandler(writer http.ResponseWriter, request *http.Request) {
 	// Write the response
 	io.WriteString(writer, "Hello, world!\n")
 	// https://owasp.org/www-community/attacks/Log_Injection
-	urlPath := request.URL.Path
-	escapedUrlPath := strings.Replace(urlPath, "\n", "", -1)
-	escapedUrlPath = strings.Replace(escapedUrlPath, "\r", "", -1)
+	urlPath = strings.Replace(urlPath, "\n", "", -1)
+	urlPath = strings.Replace(urlPath, "\r", "", -1)
 	// Get the path from the request.
-	fmt.Println(escapedUrlPath)
+	fmt.Println(urlPath)
 }
