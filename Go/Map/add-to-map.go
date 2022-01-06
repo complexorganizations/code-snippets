f70586248216d72f8875d7f181b0ca7269b51ac6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-05 20:55:29 -0500

    Update add-to-map.go

--- Go/Map/add-to-map.go
+++ Go/Map/add-to-map.go
@@ -8,11 +8,11 @@ func main() {
 	// Create a map with a key of type string with a value of type string.
 	randomMap := make(map[string]string)
 	// Add a key-value pair to the map.
-	fmt.Println(addPairToMap(randomMap, "Name", "John"))
+	fmt.Println(addKeyValueToMap(randomMap, "Name", "John"))
 }
 
-// Add a key-value pair to the map.
-func addPairToMap(providedMap map[string]string, key string, value string) map[string]string {
+// Add a key-value pair to the given map.
+func addKeyValueToMap(providedMap map[string]string, key string, value string) map[string]string {
 	providedMap[key] = value
 	return providedMap
 }
