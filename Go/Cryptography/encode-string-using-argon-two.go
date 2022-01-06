768a995fc8af2db4276c13f38ffd23aa78b2efd1
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-06 13:18:04 -0500

    simplify the code.

--- Go/Cryptography/encode-string-using-argon-two.go
+++ Go/Cryptography/encode-string-using-argon-two.go
@@ -5,57 +5,23 @@ import (
 	// "encoding/hex"
 	"fmt"
 	// "log"
-	// "math/big"
 	// "golang.org/x/crypto/argon2"
 )
 
-/*
-// The config for Argon2
-type argonConfig struct {
-	keyLength uint32
-	memory    uint32
-	salt      []byte
-	threads   uint8
-	time      uint32
-}
-
-var argonConfiguration = &argonConfig{
-	keyLength: uint32(randomIntBetween(1024*1, 1024*32)),
-	memory:    uint32(randomIntBetween(1024*512, 1024*1024)),
-	salt:      make([]byte, randomIntBetween(1024*1, 1024*32)),
-	threads:   uint8(randomIntBetween(32*1, 32*5)),
-	time:      uint32(randomIntBetween(60*1, 60*5)),
-}
-*/
-
 func main() {
+	// Using argon two encode bytes
+	// fmt.Println(encodeByteUsingArgonTwo([]byte("WHV02Qkhv7JhD8g4YFt8"), 16, 32, 32*1024, 4, 3))
 	fmt.Println("Hello, World!")
-	// Generate a hash of the given password
-	// fmt.Println(encodeStringUsingArgonTwo(argonConfiguration, "WHV02Qkhv7JhD8g4YFt8"))
 }
 
 /*
-func encodeStringUsingArgonTwo(argonSettings *argonConfig, password string) string {
-	// Generate a random salt
-	_, err := rand.Read(argonSettings.salt)
+// Encode bytes using argon two
+func encodeByteUsingArgonTwo(password []byte, saltLen int64, keyLen uint32, memory uint32, threads uint8, time uint32) string {
+	var salt = make([]byte, saltLen)
+	_, err := rand.Read(salt)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// Generate a hash
-	return fmt.Sprintf("%d,%d,%d,%s,%s", argonSettings.memory, argonSettings.time, argonSettings.threads, hex.EncodeToString(argonSettings.salt), hex.EncodeToString(argon2.IDKey([]byte(password), argonSettings.salt, argonSettings.time, argonSettings.memory, argonSettings.threads, argonSettings.keyLength)))
-}
-
-// Generate a random int between two numbers
-func randomIntBetween(min int, max int) int {
-	return generateRandomInt(int64(max-min)) + min
-}
-
-// Generate a random int between 0 and a given max
-func generateRandomInt(max int64) int {
-	randomInt, err := rand.Int(rand.Reader, big.NewInt(max))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return int(randomInt.Int64())
+	return fmt.Sprintf("%d,%d,%d,%s,%s", memory, time, threads, hex.EncodeToString(salt), hex.EncodeToString(argon2.IDKey(password, salt, time, memory, threads, keyLen)))
 }
 */
