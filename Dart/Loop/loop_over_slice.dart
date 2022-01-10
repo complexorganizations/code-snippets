8fd3c9f4d681c5bface44983244d6ffd09982987
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-10 02:12:49 -0500

    Create loop_over_slice.dart

---
+++ Dart/Loop/loop_over_slice.dart
@@ -0,0 +1,10 @@
+import "dart:core";
+
+void main() {
+    // Create a slice of strings
+    var fruits = ["Apple", "Orange", "Pear"];
+    // Loop over the slice
+    for (var fruit in fruits) {
+        print(fruit);
+    }
+}
