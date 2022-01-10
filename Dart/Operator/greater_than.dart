4248c038fb7ad61c8c775bece5822a21e51d3f2c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-10 02:45:07 -0500

    Create greater_than.dart

---
+++ Dart/Operator/greater_than.dart
@@ -0,0 +1,11 @@
+import "dart:core";
+
+void main() {
+  // Check if the first value is greater than the second value.
+  print(firstGreaterThanSecond(10, 5));
+}
+
+// Check if the first value is greater than the second value.
+bool firstGreaterThanSecond(final int primary, final int secondary) {
+  return primary > secondary;
+}
