89f287550378eb52e57d383a5d10327f5ce43b78
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-11 17:10:14 -0500

    Update split_string_by_delimiter.dart

--- Dart/String/split_string_by_delimiter.dart
+++ Dart/String/split_string_by_delimiter.dart
@@ -2,7 +2,7 @@ import "dart:core";
 
 void main() {
   // Split a given string using a given delimiter
-  print(splitStringUsingDelimiter("012345-67890", "012345-"));
+  print(splitStringUsingDelimiter("012345-67890", "-"));
 }
 
 // Split a given string using a given delimiter
