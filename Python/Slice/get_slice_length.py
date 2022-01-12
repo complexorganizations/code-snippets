b16a1db6880ca1ae1e471b178e2d40c846f8c9c6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 00:40:23 -0500

    Create get_slice_length.py

---
+++ Python/Slice/get_slice_length.py
@@ -0,0 +1,11 @@
+# Get the slice length.
+def get_slice_length(provided_slice):
+    return len(provided_slice)
+
+def main():
+    # Make a slice using a random set of data.
+    randomSlice = ["c", "a", "b"]
+    # Get the slice length.
+    print(get_slice_length(randomSlice))
+
+main()
