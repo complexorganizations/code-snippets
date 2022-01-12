76160c1b024da631be0b02e02f90dd160bd3b805
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 17:51:56 -0500

    Create get_value_of_key_in_map.py

---
+++ Python/MAP/get_value_of_key_in_map.py
@@ -0,0 +1,13 @@
+# Get the value of a given key in a map.
+def get_value_of_key_in_map(provided_map, provided_key):
+    return provided_map.get(provided_key)
+
+
+def main():
+    # Make a map using a random set of data.
+    randomMap = {"a": 1, "b": 2, "c": 3}
+    # Get the value of a given key in a map.
+    print(get_value_of_key_in_map("a"))
+
+
+main()
