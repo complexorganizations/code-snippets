4d7b61184abd9e77441e9763797e526125577bce
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 07:35:26 -0500

    Update and rename change_element_at_slice.py to change_element_at_slice_via_index.py

--- Python/Slice/change_element_at_slice_via_index.py
+++ Python/Slice/change_element_at_slice_via_index.py
@@ -1,5 +1,5 @@
 # Reverse the slice order.
-def change_element_at_slice(provided_slice, provided_index, provided_element):
+def change_element_at_slice_via_index(provided_slice, provided_index, provided_element):
     provided_slice[provided_index] = provided_element
     return provided_slice
 
@@ -7,6 +7,6 @@ def main():
     # Make a slice using a random set of data.
     randomSlice = ["a", "b", "c"]
     # Reverse the slice order.
-    print(change_element_at_slice(randomSlice, 1, "e"))
+    print(change_element_at_slice_via_index(randomSlice, 1, "e"))
 
 main()
