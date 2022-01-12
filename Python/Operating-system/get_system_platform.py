b64e28b40b7c3f54bac01b90ddf959b194e37573
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 12:46:57 -0500

    Create get_system_platform.py

---
+++ Python/Operating-system/get_system_platform.py
@@ -0,0 +1,14 @@
+import platform
+
+
+# Get the current system platform
+def get_system_platform():
+    return platform.system()
+
+
+def main():
+  # Get the current system platform.
+  print(get_system_platform())
+
+
+main()
