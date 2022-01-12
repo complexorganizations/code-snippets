1b971b06aca306f4d92894045b1cffe27414d19b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 00:31:03 -0500

    Create remove_element_from_slice.py

---
+++ Python/Slice/remove_element_from_slice.py
@@ -0,0 +1,12 @@
+# Remove a specific element from a slice.
+def remove_element_from_slice(provided_slice, provided_element):
+    provided_slice.remove(provided_element)
+    return provided_slice
+
+def main():
+    # Make a slice using a random set of data.
+    randomSlice = ["c", "a", "b"]
+    # Remove a specific element from a slice.
+    print(remove_element_from_slice(randomSlice, "a"))
+
+main()
