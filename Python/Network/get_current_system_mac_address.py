eef0672af5c0533f4eb6bd48db7cafcea9f5af96
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 11:45:21 -0500

    Create get_current_system_mac_address.py

---
+++ Python/Network/get_current_system_mac_address.py
@@ -0,0 +1,15 @@
+import uuid
+
+
+
+# Get the current system MAC address
+def get_current_system_mac_address():
+    return ":".join(["{:02x}".format((uuid.getnode() >> i) & 0xff) for i in range(0, 8*6, 8)][::-1])
+
+
+def main():
+    # Get the current system MAC address
+    print(get_current_system_mac_address())
+
+
+main()
