afff8c0932d4483ab14110806dd3e42bb5039ac8
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 12:37:01 -0500

    Create get_current_operating_system_name.py

---
+++ Python/Operating-system/get_current_operating_system_name.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Get the current operating system name.
+def get_current_operating_system_name():
+  return os.name
+
+
+def main():
+  # Get the current operating system name.
+  print(get_current_operating_system_name())
+
+
+main()
