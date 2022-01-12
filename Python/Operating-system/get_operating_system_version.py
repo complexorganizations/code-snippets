9e0dc82c4759065b25a67f220038f08e0563697d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 12:50:02 -0500

    Create get_operating_system_version.py

---
+++ Python/Operating-system/get_operating_system_version.py
@@ -0,0 +1,15 @@
+import platform
+
+
+# Get the current operating system version
+def get_operating_system_version():
+    return platform.version()
+
+
+def main():
+  # Get the current operating system version
+  print(get_operating_system_version())
+
+
+
+main()
