ca162df529be38bb2868a59a880a7ae2b3d8f7d3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 11:12:01 -0500

    Update decode_json.py

--- Python/JSON/decode_json.py
+++ Python/JSON/decode_json.py
@@ -2,7 +2,7 @@ import json
 
 
 # Parse JSON and return a object
-def json_decode(provided_json):
+def decode_json(provided_json):
     return json.loads(provided_json)
 
 
@@ -10,7 +10,7 @@ def main():
     # Random JSON as example
     testJSON = '{"name":"John", "age":30, "city":"New York"}'
     # Parse JSON and return a object
-    jsonContent = json_decode(testJSON)
+    jsonContent = decode_json(testJSON)
     print(jsonContent["name"])
 
 
