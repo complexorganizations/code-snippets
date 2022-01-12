770e113015ce34435a8fb788debf174d4e18989a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 00:36:41 -0500

    Create get_random_element_from_slice.py

---
+++ Python/Slice/get_random_element_from_slice.py
@@ -0,0 +1,13 @@
+import random
+
+# Pick an element at random from the slice.
+def get_random_element_from_slice(provided_slice):
+    return provided_slice[random.randint(0, len(provided_slice) - 1)]
+
+def main():
+    # Make a slice using a random set of data.
+    randomSlice = ["c", "a", "b"]
+    # Add a new element to the slice.
+    print(get_random_element_from_slice(randomSlice))
+
+main()
