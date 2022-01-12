939da7e6304079ace3e14b7154f2db58fa987bd4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-11 21:15:27 -0500

    Update get_url_user_info.dart

--- Dart/URL/get_url_user_info.dart
+++ Dart/URL/get_url_user_info.dart
@@ -7,6 +7,5 @@ void main() {
 
 // Get the user info from a given url.
 String getUserInfoFromURL(final String url) {
-  final Uri parsedURL = Uri.parse(url);
-  return parsedURL.userInfo;
+  return Uri.parse(url).userInfo;
 }
