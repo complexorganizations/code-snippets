aad368c4d11644b671791615346762fa0b41739a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 19:36:19 -0500

    Create get_all_documents_in_directory.py

---
+++ Python/Directory/get_all_documents_in_directory.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Get all the documents in a given directory.
+def get_all_documents_in_directory(system_path)
+    return os.listdir(system_path)
+
+
+def main():
+    # Get all the documents in a given directory 
+    print(get_all_documents_in_directory("assets/valid/aDT5V7223266kFX4uv9P9o7ovi6tdWhv"))
+
+
+main()
