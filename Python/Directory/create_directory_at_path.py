4a39bdfa483a12cb2633d3f0a7f0a0e5c7b3c68c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 19:20:40 -0500

    Create create_directory_at_path.py

---
+++ Python/Directory/create_directory_at_path.py
@@ -0,0 +1,13 @@
+import os
+
+
+# Create a directory at a given path.
+def create_directory_at_path(system_path):
+  return os.mkdir(system_path)
+
+
+def main():
+    # Create a directory at a given path.
+    print(create_directory_at_path("assets/remove/rK836Z7oekTE7n9YHL2898Bi2"))
+
+main()
