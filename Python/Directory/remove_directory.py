6bd33b231f4988dbe50c4263c58ab36fcff4fff8
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-12 19:30:01 -0500

    Create remove_directory.py

---
+++ Python/Directory/remove_directory.py
@@ -0,0 +1,12 @@
+import os
+
+# Remove a directory.
+def remove_directory(system_path):
+    os.rmdir(system_path)
+
+
+def main():
+    # Remove a directory
+    remove_directory("assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9")
+
+main()
