9e8ccc9685def549542bc78e298e96d3dce2ee67
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-13 16:00:30 -0500

    Create append_write_to_file.py

---
+++ Python/File/append_write_to_file.py
@@ -0,0 +1,13 @@
+# Append and write some content to a file.
+def append_write_to_file(system_path, content):
+    with open(system_path "w") as file:
+        file.write(content)
+
+
+
+def main():
+    # Append and write some content to a file.
+    append_write_to_file("assets/remove/zVA3m3BEJ5wSe45v93Fi8788z4GoKjnW/tEU29kAcG8m5C4y268tmJFH35" "Hello, World!")
+
+
+main()
