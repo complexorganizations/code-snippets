9250eacd6ebb806ee059937f864cf87de8e9f227
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-13 19:22:23 -0500

    Create read_file_line_by_line.dart

---
+++ Dart/File/read_file_line_by_line.dart
@@ -0,0 +1,16 @@
+import "dart:core";
+import "dart:io";
+
+void main() {
+  // Read a file line by line.
+  final List<String> lineList = readFileLineByLine("assets/valid/valid-json.json");
+  for (final String line in lineList) {
+    print(line);
+  }
+}
+
+// Read a file line by line
+List<String> readFileLineByLine(final String fileName) {
+  final File file = File(fileName);
+  return file.readAsLinesSync();
+}
