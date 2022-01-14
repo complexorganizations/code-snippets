95a1b6fa3e5b6e3d7d9d85b87a41f1c89f6838a0
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-13 22:24:31 -0500

    Create get_sha_512_of_string.dart

---
+++ Dart/Cryptography/get_sha_512_of_string.dart
@@ -0,0 +1,12 @@
+import "dart:convert";
+import "package:crypto/crypto.dart";
+
+void main() {
+  // Get the sha-512 of a given string.
+  print(getSHA512OfString("Hello, World!"));
+}
+
+// Get the SHA-512 of a given string.
+Digest getSHA512OfString(final String content) {
+  return sha512.convert(utf8.encode(content));
+}
