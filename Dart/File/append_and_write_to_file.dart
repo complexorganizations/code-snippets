97ee4130d9342fb25947ec4fe73cbb79d7cd00ee
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-13 19:11:43 -0500

    Create append_and_write_to_file.dart

---
+++ Dart/File/append_and_write_to_file.dart
@@ -0,0 +1,13 @@
+import "dart:core";
+import "dart:io";
+
+void main() {
+  // Append and write to a file
+  appendAndWriteToFile("assets/valid/j3U5uJY779L49q98MX86iFsxs2kY9ew3/P55oRK95G755P7j3TqGa7G52Y", "m2fp2B27B5AoY3t3Qo5NP4968");
+}
+
+// Append and write some content to a file.
+void appendAndWriteToFile(final String fileName, final String content) {
+  final File file = File(fileName);
+  file.writeAsStringSync(content, mode: FileMode.append);
+}
