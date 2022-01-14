45c307191f4554e37f905e1096ccde554a30abb4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-14 16:47:46 -0500

    Update connectivity_check.dart

--- Dart/Network/connectivity_check.dart
+++ Dart/Network/connectivity_check.dart
@@ -9,29 +9,14 @@ Future<void> main() async {
 
 // Check if there is network on the current system.
 Future<bool> connectivityCheck() async {
-  bool connectivity = false;
-  final List<String> domainLists = <String>[
-    "amazon.com",
-    "apple.com",
-    "cloudflare.com",
-    "facebook.com",
-    "github.com",
-    "google.com",
-    "reddit.com",
-    "twitter.com",
-    "wikipedia.org",
-    "youtube.com",
-  ];
-  for (final String domain in domainLists) {
-    try {
-      final List<InternetAddress> response =
-          await InternetAddress.lookup(domain);
-      if (response.isNotEmpty) {
-        connectivity = true;
-      }
-    } on SocketException {
-      connectivity = false;
+  try {
+    final List<InternetAddress> response =
+        await InternetAddress.lookup("example.com");
+    if (response.isNotEmpty) {
+      return true;
     }
+  } on SocketException {
+    return false;
   }
-  return connectivity;
+  return false;
 }
