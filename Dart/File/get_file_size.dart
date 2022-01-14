1517987bea9df0451371189078eff24529b823cd
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-13 19:25:18 -0500

    Create get_file_size.dart

---
+++ Dart/File/get_file_size.dart
@@ -0,0 +1,13 @@
+import "dart:core";
+import "dart:io";
+
+void main() {
+  // Get the size of a given file.
+  print(getFileSize("assets/remove/zVA3m3BEJ5wSe45v93Fi8788z4GoKjnW/README.md"));
+}
+
+// Get the size of a given file.
+int getFileSize(final String systemFile) {
+  final File file = File(systemFile);
+  return file.lengthSync();
+}
