213138f441c7dd6961b1d969229b0d215f39bd65
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-15 07:55:57 -0500

    Create get_current_month.cpp

---
+++ C++/Time/get_current_month.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Get the current month in the year.
+int getCurrentMonth() {
+    time_t t;
+    struct tm *tm;
+    time(&t);
+    tm = localtime(&t);
+    return tm->tm_mon + 1;
+}
+
+int main() {
+    std::cout << getCurrentMonth() << std::endl;
+    return 0;
+}
