583a73600cead113814cd61227ad75fd774d7eb9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-15 07:33:43 -0500

    Update primary_less_than_or_equal_secondary.cpp

--- C++/Math/primary_less_than_or_equal_secondary.cpp
+++ C++/Math/primary_less_than_or_equal_secondary.cpp
@@ -5,7 +5,6 @@ bool primaryLessThanOrEqualToSecondary(int primary, int secondary) {
     return primary <= secondary;
 }
 
-
 int main() {
     // Check if the primary value is less than or equal to the secondary value.
     printf("%d \n", primaryLessThanOrEqualToSecondary(1, 10));
