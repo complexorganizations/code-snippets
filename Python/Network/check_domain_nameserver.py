9b794eb959aa71c07df2d0fb375e5e86e703139e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-15 12:07:09 -0500

    Create check_domain_nameserver.py

---
+++ Python/Network/check_domain_nameserver.py
@@ -0,0 +1,16 @@
+import dns.resolver
+
+
+# Check a domain's name servers.
+def check_domain_nameserver(provided_domain):
+    return dns.resolver.resolve(provided_domain, "NS")
+
+
+def main():
+    # Check a domain's name servers.
+    websiteNameServers = checkDomainNameServer("example.com")
+    for nameservers in websiteNameServers:
+        print(nameservers)
+
+
+main()
