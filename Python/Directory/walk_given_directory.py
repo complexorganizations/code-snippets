82b29f88229e37a1a235522a8334e14d34e53eed
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 14:03:16 -0500

    Create walk_given_directory.py

---
+++ Python/Directory/walk_given_directory.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+# Walk a directory and return everything.
+def walkGivenDirectory(system_path):
+    for root, directory, files in os.walk(system_path):
+        return root, directory, files
+
+
+def main():
+    # Walk a directory
+    print(walkGivenDirectory("assets/valid/aDT5V7223266kFX4uv9P9o7ovi6tdWhv/"))
+
+
+
+main()
