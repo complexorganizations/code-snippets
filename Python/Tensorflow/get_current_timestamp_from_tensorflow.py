edc1e291bfe62f043d7dc52a2746121360fabcd7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 16:31:41 -0500

    Update and rename time.py to get_current_timestamp_from_tensorflow.py

--- Python/Tensorflow/get_current_timestamp_from_tensorflow.py
+++ Python/Tensorflow/get_current_timestamp_from_tensorflow.py
@@ -1,13 +1,12 @@
 import tensorflow
 
 
+# Get the current timestamp using tensorflow.
 def get_current_timestamp_from_tensorflow():
-    """Get the current timestamp using tensorflow."""
     return tensorflow.timestamp()
 
 
 def main():
-    """Run the main function."""
     # Print the current time using tensorflow
     print(get_current_timestamp_from_tensorflow())
 
