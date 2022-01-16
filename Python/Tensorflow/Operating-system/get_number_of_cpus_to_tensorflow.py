29ba7f6226ae8b24f04d538d2338888f68bfee1e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 16:47:17 -0500

    Create get_number_of_cpus_to_tensorflow.py

---
+++ Python/Tensorflow/Operating-system/get_number_of_cpus_to_tensorflow.py
@@ -0,0 +1,17 @@
+import tensorflow
+
+
+
+
+# Get a number of all available cpus to tensorflow.
+def get_number_of_cpus_to_tensorflow():
+    return len(tensorflow.config.list_physical_devices("CPU"))
+
+
+
+def main():
+    # Get a number of all available cpus to tensorflow.
+    print(get_number_of_cpus_to_tensorflow())
+
+
+main()
