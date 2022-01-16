2a7c19cd86a318d0d10f155cab92cf889491136d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 02:10:51 -0500

    Create loop_over_map.py

---
+++ Python/Loop/loop_over_map.py
@@ -0,0 +1,15 @@
+# Loop over a map.
+def loop_over_map():
+    randomMap = {"Name": "John Doe", "Age": 25, "Gender": "Male"}
+    for key, value in randomMap.items():
+        print(key, value)
+
+
+
+def main():
+    # Loop over a map.
+    loop_over_map()
+
+
+
+main()
