e25f1429e559e25f1ddfec4c3a3d9cda0ff2b14a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 16:51:41 -0500

    Create get_number_of_gpus_to_tensorflow.py

---
+++ Python/Tensorflow/Operating-system/get_number_of_gpus_to_tensorflow.py
@@ -0,0 +1,16 @@
+import tensorflow
+
+
+
+# Get a number of all avialable gpus to tensorflow.
+def get_number_of_gpus_to_tensorflow():
+    return len(tensorflow.config.list_physical_devices("GPU"))
+
+
+
+def main():
+    # Get a number of all avialable gpus to tensorflow.
+    print(get_number_of_gpus_to_tensorflow())
+
+
+main()
