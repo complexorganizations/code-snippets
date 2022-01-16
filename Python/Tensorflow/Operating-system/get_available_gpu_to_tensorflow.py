7c28a6cd79ddcc3d65d555332f0beba3a57bcfdf
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 16:48:45 -0500

    Create get_available_gpu_to_tensorflow.py

---
+++ Python/Tensorflow/Operating-system/get_available_gpu_to_tensorflow.py
@@ -0,0 +1,16 @@
+import tensorflow
+
+
+
+# Get a list of all the GPUs available to TensorFlow.
+def get_available_gpu_to_tensorflow():
+    return tensorflow.config.list_physical_devices("GPU")
+
+
+
+def main():
+    # Get a list of all the GPUs available to TensorFlow.
+    print(get_available_gpu_to_tensorflow())
+
+
+main()
