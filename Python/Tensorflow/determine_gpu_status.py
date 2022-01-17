28c6036e4d1b8bcf57f57a4bcecec8ddfb4f9c29
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 19:05:07 -0500

    Create determine_gpu_status.py

---
+++ Python/Tensorflow/determine_gpu_status.py
@@ -0,0 +1,20 @@
+import os
+
+
+
+# Choose to either enable or disable the GPU use.
+def determine_gpu_status(enable_or_disable_gpu):
+    if enable_or_disable_gpu == True:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    elif enable_or_disable_gpu False:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+
+def main():
+    # Choose the status of the GPU.
+    determine_gpu_status(True)
+
+
+
+main()
