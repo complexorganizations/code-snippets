4177d111c09bc8621ad023167984b22a07d7cbc5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-16 19:01:12 -0500

    Create get_rocm_status_from_tensorflow.py

---
+++ Python/Tensorflow/get_rocm_status_from_tensorflow.py
@@ -0,0 +1,16 @@
+import tensorflow
+
+
+# Get the status of rocm from tensorflow.
+def get_rocm_status_from_tensorflow():
+    return tensorflow.test.is_built_with_rocm()
+  
+  
+
+def main():
+    # Get the status of rocm from tensorflow.
+    print(get_rocm_status_from_tensorflow())
+
+
+
+main()
