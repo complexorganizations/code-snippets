4d5ecfdbc14047d5a86668f3232dc1a1757d689e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-17 10:44:17 -0500

    Create get_length_of_slice.rs

---
+++ Rust/src/Slice/get_length_of_slice.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Create a slice of random elements
+    let random_list_of_elements = ["one", "two", "three", "four", "five"];
+    // Get the length of the slice
+    println!("{}", get_length_of_slice(&random_list_of_elements));
+}
+
+// Get the length of a given slice.
+fn get_length_of_slice(provided_slice: &[&str]) -> usize {
+    return provided_slice.len();
+}
