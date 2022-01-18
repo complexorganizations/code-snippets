9012ac193e26f10d2d877b9d2b188afad637cacc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-18 14:09:18 -0500

    Create subtract_two_numbers.rs

---
+++ Rust/src/Math/subtract_two_numbers.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Subtract two numbers
+    println!("5 - 3 = {}", subtract_two_numbers(5, 3));
+}
+
+// Subtract two numbers and than return the difference.
+fn subtract_two_numbers(primary: i32, secondary: i32) -> i32 {
+    return primary - secondary;
+}
