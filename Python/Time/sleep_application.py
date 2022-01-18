15e26c520bd9f69e5220790484fa38a1ead64291
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-18 00:13:30 -0500

    Create  sleep_application.py

---
+++ Python/Time/sleep_application.py
@@ -0,0 +1,15 @@
+import time
+
+
+
+# Sleep the application for a given ammount of time.
+def sleep_application(provided_time):
+  time.sleep(provided_time)
+
+
+def main():
+  # Sleep the application for a given ammount of time.
+  sleep_application(30)
+
+
+main()
