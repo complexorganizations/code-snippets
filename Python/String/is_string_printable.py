857e169b890c59b5c68f8ee19686f669422b85f5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-18 06:40:08 -0500

    Create is_string_printable.py

---
+++ Python/String/is_string_printable.py
@@ -0,0 +1,11 @@
+# Check if a given string is printable.
+def is_string_printable(provided_string):
+        return provided_string.isprintable()
+    
+
+def main():
+        # Check if a given string is printable.
+        print(is_string_printable("\n"))
+
+
+main()
