0cec646d18e89dbd3bff096b788d6193b83e57ce
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-18 05:19:02 -0500

    Create get_file_content_type.py

---
+++ Python/File/get_file_content_type.py
@@ -0,0 +1,17 @@
+import mimetypes
+
+
+
+# Get the content type of a file.
+def get_file_content_type(system_path):
+    return mimetypes.guess_type(system_path)
+
+
+
+def main():
+    # Get the content type of a file.
+    print(get_file_content_type("assets/valid/valid-json.json"))
+
+
+
+main()
