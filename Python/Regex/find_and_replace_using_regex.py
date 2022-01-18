0473511d6beab336ba56e8db51ae8da4b1d7d83d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-18 06:13:42 -0500

    Create find_and_replace_using_regex.py

---
+++ Python/Regex/find_and_replace_using_regex.py
@@ -0,0 +1,17 @@
+import re
+
+
+
+# Find and replace in a string using regex.
+def find_and_replace_using_regex(provided_regex, provided_replace, provided_content):
+        return re.sub(provided_regex, provided_replace, provided_content)
+
+
+
+def main():
+        # Find and replace using regex.
+        print(find_and_replace_using_regex("Hello", "hello", "Hello, World!"))
+
+
+
+main()
