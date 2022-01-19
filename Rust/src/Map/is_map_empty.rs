745255bb59df0b1c4c92e2dc16c3874ef1209fc3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-19 16:42:48 -0500

    Create is_map_empty.rs

---
+++ Rust/src/Map/is_map_empty.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+fn main() {
+  // Create a new map.
+  let mut generated_map = HashMap::new();
+  // Add random key value pair to a given map.
+  generated_map.insert("a", "1");
+  generated_map.insert("b", "2");
+  generated_map.insert("c", "3");
+  // Get the length of a map.
+  println!("{}", is_map_empty(&mut generated_map));
+}
+
+// Check if the given map is empty.
+fn is_map_empty(map: &HashMap<&str, &str>) -> bool {
+    return map.is_empty();
+}
