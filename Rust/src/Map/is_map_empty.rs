e1a7741a700ff60553f01cf294b21a298ba57a4e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-19 16:49:59 -0500

    Update is_map_empty.rs

--- Rust/src/Map/is_map_empty.rs
+++ Rust/src/Map/is_map_empty.rs
@@ -7,7 +7,7 @@ fn main() {
   generated_map.insert("a", "1");
   generated_map.insert("b", "2");
   generated_map.insert("c", "3");
-  // Get the length of a map.
+  // Check if a map is empty.
   println!("{}", is_map_empty(&mut generated_map));
 }
 
