ff8eefe6bb6fb19860f9d5c3bafb8cea789f3da2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-20 11:46:04 -0500

    Create is_number_even.dart

---
+++ Dart/Math/is_number_even.dart
@@ -0,0 +1,11 @@
+import "dart:core";
+
+void main() {
+  // To determine if a given number is even.
+  print(isNumberEven(2));
+}
+
+// Determine whether a given integer is even.
+bool isNumberEven(final int number) {
+  return number.isEven;
+}
