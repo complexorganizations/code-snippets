434f075d838fc08113a7ded31b616a83a937f96f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-22 14:00:56 -0500

    Create copy_pair_from_primary_to_secondary_map.dart

---
+++ Dart/MAP/copy_pair_from_primary_to_secondary_map.dart
@@ -0,0 +1,23 @@
+import "dart:core";
+
+void main() {
+  // Create two maps.
+  final Map<dynamic, dynamic> firstMap = <dynamic, dynamic>{
+    "name": "John",
+  };
+  final Map<dynamic,dynamic> secondMap = <dynamic, dynamic>{
+    "age": 30,
+    "isMarried": false
+  };
+  // Copy the first map to the second.
+  print(copyPairFromPrimaryToSecondaryMap(firstMap, secondMap));
+}
+
+// Copy all the pair from the primary to secondary map.
+Map<dynamic, dynamic> copyPairFromPrimaryToSecondaryMap(
+  final Map<dynamic, dynamic> primaryMap,
+  final Map<dynamic, dynamic> secondaryMap,
+) {
+  secondaryMap.addAll(primaryMap);
+  return secondaryMap;
+}
