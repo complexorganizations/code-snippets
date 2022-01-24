38d0bb448b4711a95dc6fcb24e7fc70482099bf1
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-24 09:18:35 -0500

    Create get_smallest_number_in_slice.dart

---
+++ Dart/Math/get_smallest_number_in_slice.dart
@@ -0,0 +1,14 @@
+import "dart:core";
+import "dart:math";
+
+void main() {
+  // Create of list of numbers.
+  final List<int> randomSlice = [0, 1, 2, 3];
+  // Find the smallest number in the given slice.
+  print(getSmallestNumberInSlice(randomSlice));
+}
+
+// Get the smallest number in a given slice.
+int getSmallestNumberInSlice(final List<int> givenSlice) {
+  return givenSlice.reduce(min);
+}
