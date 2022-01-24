088730c35b68362a41341226172bcac9d0fe8873
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-24 09:15:54 -0500

    Create get_biggest_number_in_slice.dart

---
+++ Dart/Math/get_biggest_number_in_slice.dart
@@ -0,0 +1,13 @@
+import "dart:math";
+
+void main() {
+  // Create of list of numbers.
+  final List<int> randomSlice = [1, 2, 3];
+  // Find the biggest number in the given slice.
+  print(getBiggestNumberInSlice(randomSlice));
+}
+
+// Get the biggest number in a given slice.
+int getBiggestNumberInSlice(final List<int> givenSlice) {
+  return givenSlice.reduce(max);
+}
