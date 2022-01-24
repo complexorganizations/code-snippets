61faa7295ea9237863fe41d4266092ee678bd4c7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-23 21:01:58 -0500

    Update get_file_extension.dart

--- Dart/File/get_file_extension.dart
+++ Dart/File/get_file_extension.dart
@@ -1,16 +1,12 @@
 import "dart:core";
+import "package:path/path.dart";
 
 void main() {
   // Get the file extension of a file name.
   print(getFileExtension("assets/valid/valid-json.json"));
 }
 
-// Get the file extension of a file name.
-String? getFileExtension(final String fileName) {
-  final int index = fileName.lastIndexOf(".");
-  if (index == -1) {
-    return null;
-  } else {
-    return fileName.substring(index + 1);
-  }
+// Get a file's extension.
+String getFileExtension(final String filePath) {
+  return extension(filePath);
 }
