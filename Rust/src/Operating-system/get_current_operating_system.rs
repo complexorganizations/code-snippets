9a76135a188857b9d8399f95ab67ff354c0d0308
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-25 16:12:24 -0500

    Create get_current_operating_system.rs

---
+++ Rust/src/Operating-system/get_current_operating_system.rs
@@ -0,0 +1,12 @@
+#[allow(clippy::needless_return)]
+use std::env;
+
+fn main() {
+    // Get the current operating system
+    println!("{}", get_current_operating_system());
+}
+
+// Get the current operating system and return it.
+fn get_current_operating_system() -> &'static str {
+    return env::consts::OS
+}
