01c4b68d031e9589964800f437809110f8f9f11d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-25 16:17:32 -0500

    Create is_current_operating_system_windows.rs

---
+++ Rust/src/Operating-system/is_current_operating_system_windows.rs
@@ -0,0 +1,12 @@
+#[allow(clippy::needless_return)]
+use std::env;
+
+fn main() {
+    // Check if the current operating system is windows.
+    println!("{}", is_current_operating_system_windows());
+}
+
+// Check if the current operating system is windows and return it.
+fn is_current_operating_system_windows() -> bool {
+    return env::consts::OS == "windows"
+}
