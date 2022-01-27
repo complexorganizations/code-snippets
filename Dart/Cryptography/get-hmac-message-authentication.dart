54a49a13c12a225b4d0fb26ba64f00cd8215c072
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-26 22:31:11 -0500

    Create get-hmac-message-authentication.dart

---
+++ Dart/Cryptography/get-hmac-message-authentication.dart
@@ -0,0 +1,13 @@
+import "dart:core";
+import "dart:convert";
+import "package:crypto/crypto.dart";
+
+void main() {
+// Get the hmac message authentication 
+  print(getHMACMessageAuthentication("Hello, World!", "password"));
+}
+
+// Get the hmac message authentication 
+Digest getHMACMessageAuthentication(final String content, final String password) {
+  return Hmac(sha256, utf8.encode(password)).convert(utf8.encode(password));
+}
