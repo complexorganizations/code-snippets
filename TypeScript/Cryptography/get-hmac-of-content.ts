62bf2a37a6eccb7f59f1b87c890f91d7447bec75
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-29 21:07:36 -0500

    Update get-hmac-of-content.ts

--- TypeScript/Cryptography/get-hmac-of-content.ts
+++ TypeScript/Cryptography/get-hmac-of-content.ts
@@ -1,13 +1,13 @@
 import * as crypto from "crypto"
 
 function main(): void {
-    // Get the hmac of a given content.
+    // Get the hmac of a given content
     console.log(getHMACOfContent("Hello, World!", "password"))
 }
 
 main()
 
-// Get the HMAC of a given content.
+// Get the HMAC of a given content
 function getHMACOfContent(content: string, password: string): string {
     return crypto.createHmac("sha256", password).update(content).digest("hex")
 }
