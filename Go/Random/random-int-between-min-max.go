c999d7eb742c3abd1bc90c0b5d35bd7569a2d01c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-31 13:40:56 -0500

    more optimizations

--- Go/Random/random-int-between-min-max.go
+++ Go/Random/random-int-between-min-max.go
@@ -14,14 +14,9 @@ func main() {
 
 // Generate a random int between two numbers
 func randomIntBetween(min int, max int) int {
-	return generateRandomInt(int64(max-min)) + min
-}
-
-// Generate a random int between 0 and a given max
-func generateRandomInt(max int64) int {
-	randomInt, err := rand.Int(rand.Reader, big.NewInt(max))
+	randomInt, err := rand.Int(rand.Reader, big.NewInt(int64(max)-int64(min)))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return int(randomInt.Int64())
+	return int(randomInt.Int64()) + min
 }
