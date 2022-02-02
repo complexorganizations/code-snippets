e7aec145d55cba83e66d611391d2dc4d26852e9a
Prajwal Koirala <Prajwalkoirala23@protonmail.com>
2022-02-02 02:54:48 -0500

    update

--- Dart/Cryptography/get_hmac_message_authentication.dart
+++ Dart/Cryptography/get_hmac_message_authentication.dart
@@ -1,13 +1,16 @@
-import "dart:core";
 import "dart:convert";
+import "dart:core";
 import "package:crypto/crypto.dart";
 
 void main() {
-// Get the hmac message authentication 
+// Get the hmac message authentication
   print(getHMACMessageAuthentication("Hello, World!", "password"));
 }
 
-// Get the hmac message authentication 
-Digest getHMACMessageAuthentication(final String content, final String password) {
+// Get the hmac message authentication
+Digest getHMACMessageAuthentication(
+  final String content,
+  final String password,
+) {
   return Hmac(sha256, utf8.encode(password)).convert(utf8.encode(password));
 }
