94745235b7544a61aa8651ce5bc076960b5998bc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-02-04 10:06:08 -0500

    Create validate-cidr.go

---
+++ Go/Network/validate-cidr.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// Ensure that the CIDR block is valid.
+	fmt.Println(validateCIDR("10.0.0.0/24"))
+}
+
+// Ensure that the CIDR block is valid.
+func validateCIDR(ipWithCidr string) bool {
+	_, _, err := net.ParseCIDR(ipWithCidr)
+	if err != nil {
+		return false
+	}
+	return true
+}
