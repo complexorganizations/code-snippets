7122236ef94316b93ecad0bea9cefb9f11a994db
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-02-13 21:22:04 -0500

    Update import.ts

--- TypeScript/import.ts
+++ TypeScript/import.ts
@@ -1,23 +1,23 @@
-// A simple import statement that imports a function from another file.
+// A simple import statement that imports a function from another file
 import * as os from "os"
-// Import a function from a package.
+// Import a function from a package
 import { CpuInfo } from "os"
 
 function main(): void {
-    // Get the current user name.
+    // Get the current user name
     getCurrentUser()
-    // Get the current cpu count.
+    // Get the current cpu count
     getCurrentCPUCount()
 }
 
 main()
 
-// Using the os package to get the current user name.
+// Using the os package to get the current user name
 function getCurrentUser(): void {
     console.log(os.userInfo().username)
 }
 
-// Using the os pakaage to get the current system cpu count.
+// Using the os pakaage to get the current system cpu count
 function getCurrentCPUCount(): CpuInfo[] {
     return os.cpus()
 }
