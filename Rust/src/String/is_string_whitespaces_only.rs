db83b77f5bd0a9373cbeb12f49aba86c605b179c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-02-14 22:42:39 -0500

    Create is_string_whitespaces_only.rs

---
+++ Rust/src/String/is_string_whitespaces_only.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Check if a given string is whitespaces only.
+    println!("{}", is_string_whitespaces_only("     "));
+}
+
+// Check if a string is whitespaces only.
+fn is_string_whitespaces_only(content: &str) -> bool {
+    return content.trim().is_empty();
+}
