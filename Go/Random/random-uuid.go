a63b388dc7b6b0f52b0077ce13a1bad2278d28af
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-02-25 09:37:03 -0500

    Update random-uuid.go

--- Go/Random/random-uuid.go
+++ Go/Random/random-uuid.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
-	"log"
+	
+	//"github.com/google/uuid"
 )
 
 func main() {
 	// Generate a random uuid.
-	fmt.Println(randomUUIDGenerator())
+	// fmt.Println(randomUUIDGenerator())
+	fmt.Println("Hello, World!")
 }
 
+/*
 // Generate a random uuid and return it as a string.
 func randomUUIDGenerator() string {
-	randomBytes := make([]byte, 16)
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return fmt.Sprintf("%x-%x-%x-%x-%x", randomBytes[0:4], randomBytes[4:6], randomBytes[6:8], randomBytes[8:10], randomBytes[10:])
+	return uuid.New().String()
 }
+*/
