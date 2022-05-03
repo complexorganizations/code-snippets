01ac905b25117964f4096bf16ddbdbcded26cd8a
prajwal-koirala-0000 <102563715+prajwal-koirala-0000@users.noreply.github.com>
2022-05-03 14:04:22 -0400

    Create is-domain-suffix-valid.go

---
+++ Go/Validation/is-domain-suffix-valid.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/publicsuffix"
+)
+
+func main() {
+	// Check if a given domain has a valid suffix.
+	fmt.Println(isDomainSuffixValid("www.example.com"))
+}
+
+// Check if a given domain suffix is valid.
+func isDomainSuffixValid(domain string) bool {
+	_, testICANNSuffix := publicsuffix.PublicSuffix(domain)
+	return testICANNSuffix
+}
