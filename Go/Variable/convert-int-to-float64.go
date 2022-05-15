f60b0696b83da31fc0d1103cd62fbef128ae20f9
prajwal-koirala-0000 <102563715+prajwal-koirala-0000@users.noreply.github.com>
2022-05-15 06:38:49 -0400

    Update convert-int-to-float64.go

--- Go/Variable/convert-int-to-float64.go
+++ Go/Variable/convert-int-to-float64.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-	// Convert the variable type from int to float64
+	// Convert the variable type from int to float64.
 	fmt.Println(convertIntToFloat64(10))
 }
 
-// Convert the variable type from anything to float64
+// Convert the variable type from anything to float64.
 func convertIntToFloat64(variable int) float64 {
 	return float64(variable)
 }
