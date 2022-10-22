31df73723729daab6eb53cdd615844316a4f0e96
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-22 18:28:03 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,7 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  shared_config_files = ["/users/terraform/.aws/creds"]
   shared_credentials_files = ["/users/terraform/.aws/creds"]
 }
 
