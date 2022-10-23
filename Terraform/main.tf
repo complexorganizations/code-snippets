6a42d089d435b560ece28072e1f0ffc6e98b932f
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-22 21:06:53 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region                   = "us-east-1"
   shared_credentials_files = ["assets/.env/.aws/credentials"]
 }
 
@@ -22,13 +22,19 @@ resource "aws_internet_gateway" "main_internet_gateway" {
 resource "aws_route_table" "main_route_table" {
   vpc_id = aws_vpc.main_vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_internet_gateway.id
+    cidr_block        = "0.0.0.0/0"
+    gateway_id        = aws_internet_gateway.main_internet_gateway.id
+    ipv6_ipam_pool_id = aws_vpc_ipv6_cidr_block.main_ipv6_cidr_block.ipv6_pool
   }
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id = aws_internet_gateway.main_internet_gateway.id
+    gateway_id      = aws_internet_gateway.main_internet_gateway.id
   }
+}
+
+resource "main_ipv6_cidr_block" "main_ipv6_cidr_block" {
+  vpc_id                          = aws_vpc.main_vpc.id
+  amazon_provided_ipv6_cidr_block = true
 }
 
 # route table association
@@ -39,8 +45,8 @@ resource "aws_route_table_association" "main_route_table_association" {
 
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  ipv6_cidr_block = "2001:db8:1234:1a00::/56"
+  cidr_block           = "10.0.0.0/16"
+  ipv6_cidr_block      = "2001:db8:1234:1a00::/56"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
@@ -76,13 +82,13 @@ resource "aws_security_group" "main_security_group" {
 
 # Deploy an EC2 instance
 resource "aws_instance" "main_instance" {
-  ami           = "ami-08c40ec9ead489470"
-  instance_type = "t1.micro"
-  subnet_id = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.main_security_group.id]
-  ipv6_address_count = 1
+  ami                         = "ami-08c40ec9ead489470"
+  instance_type               = "t1.micro"
+  subnet_id                   = aws_subnet.main_subnet.id
+  vpc_security_group_ids      = [aws_security_group.main_security_group.id]
+  ipv6_address_count          = 1
   associate_public_ip_address = true
-  depends_on = [aws_internet_gateway.main_internet_gateway]
+  depends_on                  = [aws_internet_gateway.main_internet_gateway]
   credit_specification {
     cpu_credits = "unlimited"
   }
