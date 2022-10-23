d7fb70490a7a2937aebaf13efed45cb29b5a2434
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-22 20:59:35 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,7 +11,6 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   shared_credentials_files = ["assets/.env/.aws/credentials"]
-  public_key = "sexample@example.com"
 }
 
 # Create the internet gateway
