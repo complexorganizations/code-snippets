0c3b13ca448e404ba978cafcb4a6662c255b9b8c
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-23 22:18:53 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -49,6 +49,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "main_subnet" {
   vpc_id                                         = aws_vpc.main_vpc.id
   cidr_block                                     = "10.0.0.0/24"
+  ipv6_cidr_block                                = "fd00:00:00::0/56"
   availability_zone                              = "us-east-1a"
   ipv6_native                                    = true
   enable_dns64                                   = true
