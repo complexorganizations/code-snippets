317d004a3afaeb24753e371b0f65563f23eb34df
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-23 21:09:51 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
-  ipv6_cidr_block   = "2001:db8:1234:1a00::/64"
+  ipv6_cidr_block   = "fd00:00:00::0/64"
 }
 
 # Create a security group
