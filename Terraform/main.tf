af6f6278dc7c5eebea1f193bee12ab1a35bb238c
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-23 21:49:44 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -42,8 +42,6 @@ resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  enable_classiclink   = true
-  enable_classiclink_dns_support = true
   enable_network_address_usage_metrics = true
 }
 
