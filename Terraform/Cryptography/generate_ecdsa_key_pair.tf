0efda853d866af8a25a5c7bf4cad79122d46582b
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-26 18:55:21 -0400

    Create generate_ecdsa_key_pair.tf

---
+++ Terraform/Cryptography/generate_ecdsa_key_pair.tf
@@ -0,0 +1,21 @@
+resource "tls_private_key" "generate_ecdsa_key_pair" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P521"
+  rsa_bits    = 2048
+}
+
+# Save the ECDSA private key in file
+resource "local_sensitive_file" "save_ecdsa_private_key" {
+  content              = tls_private_key.generate_rsa4096_key_pair.private_key_openssh
+  directory_permission = "0700"
+  file_permission      = "0700"
+  filename             = "private_key_ecdsa"
+}
+
+# Save the ECDSA public key in file
+resource "local_sensitive_file" "save_ecdsa_public_key" {
+  content              = tls_private_key.generate_rsa4096_key_pair.public_key_openssh
+  directory_permission = "0700"
+  file_permission      = "0700"
+  filename             = "public_key_ecdsa"
+}
