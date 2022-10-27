c0ba32336e06d9b6ee0c07abc4b72cf24f256373
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-26 20:25:32 -0400

    Create save_content_on_local_device.tf

---
+++ Terraform/File/save_content_on_local_device.tf
@@ -0,0 +1,5 @@
+# Save content on the local device.
+resource "local_sensitive_file" "save_content_on_local_device" {
+    content  = "ZyTdivPMmixnL9Ln5bwaT4W8sLTEPCiy"
+    filename = "/assets/remove/89mmtNQY7hM7389f48Sw46ZhbRDNQ2h9/waT4W8sL"
+}
