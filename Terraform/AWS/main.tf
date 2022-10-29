0750f224615600ef520a40c0309b6c0ed1207ec5
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-29 13:40:12 -0400

    Update main.tf

--- Terraform/AWS/main.tf
+++ Terraform/AWS/main.tf
@@ -46,6 +46,7 @@ resource "aws_route_table_association" "main_route_table_association" {
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block                           = "10.0.0.0/16"
+  instance_tenancy                     = "default"
   enable_dns_support                   = true
   enable_dns_hostnames                 = true
   assign_generated_ipv6_cidr_block     = true
