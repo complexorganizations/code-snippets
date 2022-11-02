dbe08edb6642b0b4ea684bcdca769836169d5399
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-01 20:52:52 -0400

    Save the file

--- Terraform/File/save_content_on_local_device.tf
+++ Terraform/File/save_content_on_local_device.tf
@@ -1,5 +1,7 @@
 # Save content on the local device
 resource "local_sensitive_file" "save_content_on_local_device" {
     content  = "Example content to write on the file."
-    filename = "example_file_name"
+    directory_permission = "0700"
+    file_permission      = "0700"
+    filename             = "example_file_name"
 }
