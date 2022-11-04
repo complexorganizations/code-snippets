0dd3b4eab739a2b1566463c1ff5e125bed50087d
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-04 10:22:13 -0400

    Updated the AMI image lookup tool to add more filters.

--- Terraform/AWS/main.tf
+++ Terraform/AWS/main.tf
@@ -154,11 +154,15 @@ data "aws_ami" "get_current_ubuntu_release" {
   owners      = ["099720109477"]
   filter {
     name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-jammy-22.04-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
