d48444fdedd3ae8efc10fa635a074f8af32273df
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-06 18:03:14 -0500

    Update generate_random_pet.tf

--- Terraform/Random/generate_random_pet.tf
+++ Terraform/Random/generate_random_pet.tf
@@ -1,5 +1,8 @@
 # Generate a random pet
-resource "random_pet" "generate_random_pet" {}
+resource "random_pet" "generate_random_pet" {
+    length = 2
+    separator = "-"
+}
 
 output "random_pet_output" {
     value = random_pet.generate_random_pet.id
