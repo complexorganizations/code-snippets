3601d3a5d3641ad60467ff4e1f0178be4c801798
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-20 12:19:47 -0500

    Create main.tf

---
+++ Terraform/AWS/consul/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+module "consul" {
+  source = "git@github.com:hashicorp/terraform-aws-consul.git"
+}
