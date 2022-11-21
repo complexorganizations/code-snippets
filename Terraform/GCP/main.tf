04dd527dec0a3b8cb8ead32493c749291bcaef09
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-21 01:30:10 -0500

    Update main.tf

--- Terraform/GCP/main.tf
+++ Terraform/GCP/main.tf
@@ -10,4 +10,25 @@ terraform {
 provider "google" {
   project = "github-code-snippets"
   region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
