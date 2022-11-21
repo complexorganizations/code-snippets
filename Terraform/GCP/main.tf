38d7e8ad0631011a0448447710cfac3eb9096393
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-21 01:43:02 -0500

    Update main.tf

--- Terraform/GCP/main.tf
+++ Terraform/GCP/main.tf
@@ -32,3 +32,16 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
