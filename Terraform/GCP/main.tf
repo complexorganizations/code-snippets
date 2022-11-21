69e3805ab06e88847d1e3ae1150daa6b63aa93c9
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-21 01:27:18 -0500

    Update main.tf

--- Terraform/GCP/main.tf
+++ Terraform/GCP/main.tf
@@ -1,1 +1,13 @@
-# This is a comment.
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.43.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "github-code-snippets"
+  region  = "us-central1"
+}
