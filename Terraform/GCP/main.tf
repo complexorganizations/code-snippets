199f0707d5a2ccf572a01866de9b08743697e7a6
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-21 18:07:59 -0500

    Update main.tf

--- Terraform/GCP/main.tf
+++ Terraform/GCP/main.tf
@@ -141,3 +141,40 @@ resource "google_storage_bucket" "bucket" {
   storage_class = "STANDARD"
   force_destroy = true
 }
+
+# Create a filestore instance
+resource "google_filestore_instance" "filestore" {
+  name     = "filestore-instance"
+  tier     = "STANDARD"
+  location = "us-central1-c"
+  file_shares {
+    capacity_gb = 1024
+    name        = "fileshare"
+  }
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV4"]
+  }
+}
+
+# Create a redis instance
+resource "google_redis_instance" "cache" {
+  name                    = "memory-cache"
+  tier                    = "STANDARD_HA"
+  location_id             = "us-central1-a"
+  alternative_location_id = "us-central1-b"
+  redis_version           = "REDIS_4_0"
+  display_name            = "Terraform Test Instance"
+  memory_size_gb          = 10
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = "TUESDAY"
+      start_time {
+        hours   = 0
+        minutes = 30
+        seconds = 0
+        nanos   = 0
+      }
+    }
+  }
+}
