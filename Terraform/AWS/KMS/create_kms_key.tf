2dff78fedb806ec9a9c0ea345aa7da11107dc2b2
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-20 22:18:17 -0500

    update

--- Terraform/AWS/KMS/create_kms_key.tf
+++ Terraform/AWS/KMS/create_kms_key.tf
@@ -1,13 +1,13 @@
 # Create a key using KMS
 resource "aws_kms_key" "kms_key" {
   description                        = "Create a KMS key to encrypt and decrypt files."
+  key_usage                          = "ENCRYPT_DECRYPT"
   customer_master_key_spec           = "SYMMETRIC_DEFAULT"
+  bypass_policy_lockout_safety_check = true
   deletion_window_in_days            = 7
   is_enabled                         = true
   enable_key_rotation                = true
-  bypass_policy_lockout_safety_check = true
   multi_region                       = true
-  key_usage                          = "ENCRYPT_DECRYPT"
   tags = {
     # {project-name}-kms-{0}-{us-east-1a}
     Name = "code-snippets-kms-0-us-east-1a"
