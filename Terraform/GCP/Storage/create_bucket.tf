8b65d11c3137a1def7c2dd1aa927ce77efad48b3
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-22 18:35:09 -0500

    update

--- Terraform/GCP/Storage/create_bucket.tf
+++ Terraform/GCP/Storage/create_bucket.tf
@@ -5,4 +5,7 @@ resource "google_storage_bucket" "bucket" {
   storage_class               = "STANDARD"
   force_destroy               = true
   uniform_bucket_level_access = true
+  default_event_based_hold    = false
+  requester_pays              = false
+  public_access_prevention    = "enforced"
 }
