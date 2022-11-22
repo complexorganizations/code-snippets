76e53628d72b0aac755c7321e7df69e0a8c99ca6
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-21 19:02:49 -0500

    update

--- Terraform/GCP/Firestore/create_firestore.tf
+++ Terraform/GCP/Firestore/create_firestore.tf
@@ -1,5 +1,7 @@
 # Create a firestore instance
 resource "google_firestore_document" "firestore" {
-  name     = "firestore-instance"
-  location = "us-central1"
+  name        = "firestore-instance"
+  collection  = "somenewcollection"
+  document_id = "my-doc-"
+  fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
 }
