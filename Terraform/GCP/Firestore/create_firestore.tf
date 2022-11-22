d212f97943aa99b6f7fba8c03935c0d3a8b82fa1
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-22 14:39:18 -0500

    Update create_firestore.tf

--- Terraform/GCP/Firestore/create_firestore.tf
+++ Terraform/GCP/Firestore/create_firestore.tf
@@ -1,6 +1,6 @@
-# Create a firestore instance.
+# Create a firestore instance
 resource "google_firestore_document" "firestore" {
-  collection  = "somenewcollection"
-  document_id = "my-doc-"
+  collection  = "code-snippets-database-0"
+  document_id = "code-snippets-document-"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
 }
