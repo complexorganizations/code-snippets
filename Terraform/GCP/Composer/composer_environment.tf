93002169e629f599766e48f2a1e50924355dc3bc
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-24 13:22:29 -0500

    update

--- Terraform/GCP/Composer/composer_environment.tf
+++ Terraform/GCP/Composer/composer_environment.tf
@@ -1,16 +1,13 @@
 resource "google_composer_environment" "test" {
-    name = "test"
-    region = "us-central1"
-    config {
-        node_count = 3
-        node_config {
-            zone = "us-central1-a"
-            machine_type = "n1-standard-1"
-            network = "default"
-            subnetwork = "default"
-        }
-        software_config {
-            image_version = "composer-2-airflow-2"
-        }
+  name   = "test"
+  region = "us-central1"
+  config {
+    node_count = 3
+    node_config {
+      machine_type = "n1-standard-1"
     }
+    software_config {
+      image_version = "composer-2-airflow-2"
+    }
+  }
 }
