0d6f84f16f0f5127d2f1c3f9dd63f77b11c58395
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-24 13:23:45 -0500

    update

--- Terraform/GCP/Composer/composer_environment.tf
+++ Terraform/GCP/Composer/composer_environment.tf
@@ -2,10 +2,6 @@ resource "google_composer_environment" "test" {
   name   = "test"
   region = "us-central1"
   config {
-    node_count = 3
-    node_config {
-      machine_type = "n1-standard-1"
-    }
     software_config {
       image_version = "composer-2-airflow-2"
     }
