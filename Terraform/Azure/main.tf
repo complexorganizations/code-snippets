2037961e239dd50fe0b233dd23526b42a26edfba
Prajwal Koirala <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-24 19:24:27 -0500

    update

--- Terraform/Azure/main.tf
+++ Terraform/Azure/main.tf
@@ -1,1 +1,15 @@
-# This is a comment.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.33.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "11111111-1111-1111-1111-111111111111"
+}
