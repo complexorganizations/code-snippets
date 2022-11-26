a7a28c7e0e2006bdac0db06353b6a0da2bbfe3c1
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-26 12:22:51 -0500

    Update iam_binding.tf

--- Terraform/GCP/IAM/iam_binding.tf
+++ Terraform/GCP/IAM/iam_binding.tf
@@ -1,12 +1,14 @@
 resource "google_project_iam_binding" "name" {
   role    = "roles/editor"
   project = "github-code-snippets"
-  members = ["serviceAccount:${google_service_account.example.email}",]
+  members = ["serviceAccount:${google_service_account.service_account.email}",]
 }
 
-# please create an appropriate service account for the resource
-resource "google_service_account" "example" {
-  account_id   = "<set as you like>"
-  display_name = "<set as you like>"
+# Create a google cloud service account
+resource "google_service_account" "service_account" {
+  account_id   = "iam-binding-service-account-0"
+  display_name = "iam-binding-service-account-0"
+  disabled     = false
+  description  = "The account used for iam binding to service account"
+  project      = "github-code-snippets"
 }
-
