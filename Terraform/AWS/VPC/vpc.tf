1e28e5c3d2ae8945738b22767053b7f6398ce678
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-26 12:19:33 -0500

    Update vpc.tf

--- Terraform/AWS/VPC/vpc.tf
+++ Terraform/AWS/VPC/vpc.tf
@@ -36,13 +36,21 @@ resource "aws_cloudwatch_log_group" "vpc_flow_log_group" {
   tags = {
     Name = "code-snippets-vpc-flow-log-group-0-us-east-1"
   }
-  kms_key_id = aws_kms_key.example.arn
+  kms_key_id = aws_kms_key.kms_key.arn
 }
 
-# [Shisho]: See the following document:
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
-resource "aws_kms_key" "example" {
-  description             = "example"
-  deletion_window_in_days = 10
+# Create a key using AWS KMS
+resource "aws_kms_key" "key_1" {
+  description                        = "Create a key using AWS KMS to encrypt and decrypt files."
+  key_usage                          = "ENCRYPT_DECRYPT"
+  customer_master_key_spec           = "SYMMETRIC_DEFAULT"
+  bypass_policy_lockout_safety_check = false
+  deletion_window_in_days            = 7
+  is_enabled                         = true
+  enable_key_rotation                = true
+  multi_region                       = true
+  tags = {
+    # {project-name}-kms-{0}-{us-east-1a}
+    Name = "code-snippets-kms-1-us-east-1a"
+  }
 }
-
