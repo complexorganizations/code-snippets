fb153172a44d8594306b6fc7f95447728d9d90c4
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-26 11:32:58 -0500

    Ensure not to grant permissions for users directly This pull request improves our infrastructure by fixing a security issue found by [Shisho Cloud](https://shisho.dev).
    ## Description from Shisho Cloud
    
    It is better not to grant permissions for users directly and should use groups and services accounts instead of them.

--- Terraform/GCP/IAM/iam_binding.tf
+++ Terraform/GCP/IAM/iam_binding.tf
@@ -1,7 +1,12 @@
 resource "google_project_iam_binding" "name" {
   role    = "roles/editor"
   project = "github-code-snippets"
-  members = [
-    "user:jane@example.com",
-  ]
+  members = ["serviceAccount:${google_service_account.example.email}",]
 }
+
+# please create an appropriate service account for the resource
+resource "google_service_account" "example" {
+  account_id   = "<set as you like>"
+  display_name = "<set as you like>"
+}
+
