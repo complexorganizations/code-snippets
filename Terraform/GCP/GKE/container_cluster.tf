a25aa25eb6d7afd1dd1addc838e9ef205ef99cd9
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-11-26 12:47:10 -0500

    Ensure master authorized networks of your GKE cluster is configured This pull request improves our infrastructure by fixing a security issue found by [Shisho Cloud](https://shisho.dev).
    ## Description from Shisho Cloud
    
    The master authorized networks of your GKE cluster are not configured. It's better to set them to restrict master access to a set of CIDR ranges.

--- Terraform/GCP/GKE/container_cluster.tf
+++ Terraform/GCP/GKE/container_cluster.tf
@@ -11,4 +11,10 @@ resource "google_container_cluster" "cluster" {
   node_config {
     service_account = "service-account@google.com"
   }
+  master_authorized_networks_config = [{
+    cidr_blocks = [{
+    cidr_block = "<write as you like (e.g. 10.10.128.0/24)>"
+    display_name = "<write as you like (e.g. internal)>"
+    }]
+  }]
 }
