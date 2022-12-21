599cf5dcb51913d3d7cd587671f410aeef32fc0a
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-12-21 11:51:02 -0500

    Update write-string-to-file.go

--- Go/File/write-string-to-file.go
+++ Go/File/write-string-to-file.go
@@ -10,7 +10,9 @@ func main() {
 	writeToFile("assets/remove/8aDwp8548CYD9UF3ge2C453955tdca92", []byte("Hello World"))
 }
 
-// Don't append and write to file
+/* It takes in a path and content to write to that file.
+It uses the os.WriteFile function to write the content to that file.
+It checks for errors and logs them. */
 func writeToFile(path string, content []byte) {
 	err := os.WriteFile(path, content, 0644)
 	if err != nil {
