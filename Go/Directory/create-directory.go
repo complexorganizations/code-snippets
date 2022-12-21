4bd14c6df4aff3e275e82bc179795ce322876d05
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-12-21 11:48:33 -0500

    Update create-directory.go

--- Go/Directory/create-directory.go
+++ Go/Directory/create-directory.go
@@ -10,7 +10,9 @@ func main() {
 	createDirectory("assets/remove/K44CyS7xqVt5e2V5GKCo54c9a5Y9xGxW", 0400)
 }
 
-// Create a director.
+/* The function takes two parameters: path and permission.
+We use os.Mkdir() to create the directory.
+If there is an error, we use log.Fatalln() to log the error and then exit the program. */
 func createDirectory(path string, permission os.FileMode) {
 	err := os.Mkdir(path, permission)
 	if err != nil {
